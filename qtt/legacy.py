2350b9d4c06fe5857ac39ad2abbe65631463e27a
Pieter <peendebak@tudelft.nl>
2016-06-03 17:14:22 +0200

    add loading of qtlab data

--- qtt/legacy.py
+++ qtt/legacy.py
@@ -25,4 +25,28 @@ def getODbalancepoint(od):
         bp = od['balancepointfine']
     return bp
 
+import pickle
 
+def loadpickle(pkl_file):
+    """ Load objects from file """
+    try:    
+        output = open(pkl_file, 'rb')
+        data2 = pickle.load(output)
+        output.close()
+    except:
+        if sys.version_info.major>=3:
+            # if pickle file was saved in python2 we might fix issues with a different encoding
+            output = open(pkl_file, 'rb')
+            data2 = pickle.load(output, encoding='latin')
+            #pickle.load(pkl_file, fix_imports=True, encoding="ASCII", errors="strict")
+            output.close()
+        else:
+            data2=None
+    return data2
+    
+def load_qt(fname):
+    """ Load qtlab style file """
+    alldata = loadpickle(fname)
+    if isinstance(alldata, tuple):
+        alldata = alldata[0]
+    return alldata
