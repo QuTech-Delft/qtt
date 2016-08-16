aa4c883ed8901a6de17d54586c70b7bbbee48216
lgnjanssen <L.G.N.Janssen@student.tudelft.nl>
2016-08-16 09:13:04 +0200

    Classical triple dot + example notebook for testing

--- qtt/simulation/classicaldotsystem.py
+++ qtt/simulation/classicaldotsystem.py
@@ -35,14 +35,40 @@ class ClassicalDotSystem:
 
     def makebasis(self):
         """ Define a basis of occupancy states """
-        basis = list(itertools.product(range(maxelectrons + 1), repeat=ndots))
+        basis = list(itertools.product(range(self.maxelectrons + 1), repeat=self.ndots))
         basis = np.asarray(sorted(basis, key=lambda x: sum(x)))
         self.basis = np.ndarray.astype(basis, int)
         self.nbasis = np.sum(self.basis, axis=1)
         self.Nt = len(self.nbasis)
 
     def makevars(self, varnames):
+        """ Create the variables that characterize the dot system """
         self.varnames = varnames
         for name in self.varnames:
-            exec('self.' + name + ' = 0')
+            exec('self.' + name + ' = np.zeros((self.ndots,))')
+
+
+class TripleDot(ClassicalDotSystem):
+
+    def __init__(self, name='tripledot', **kwargs):
+        super().__init__(name=name, ndots=3, ngates=3, **kwargs)
+
+        self.makebasis()
+
+        varnames = ['mu0', 'Eadd', 'W']
+        varnames += ['alpha%d' % (i+1) for i in range(self.ndots)]
+        self.makevars(varnames)
+
+        mu0_values = np.array([-27.0, -20.0, -25.0])
+        Eadd_values = np.array([54.0, 52.8, 54.0])
+        W_values = np.array([6.0, 5.0, 1.0])
+        alpha1_values = np.array([1.0, 0.25, 0.1])
+        alpha2_values = np.array([0.25, 1.0, 0.25])
+        alpha3_values = np.array([0.1, 0.25, 1.0])
+
+        for name in self.varnames:
+            exec('self.' + name + ' = ' + name + '_values')
+
+        
+
 
