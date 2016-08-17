64d4138a36edbabfad8b65fe57a1ec88ac0d94c5
Pieter <pieter.eendebak@gmail.com>
2016-08-17 10:08:19 +0200

    patch QtPlot callback

--- qtt/__init__.py
+++ qtt/__init__.py
@@ -1,8 +1,6 @@
 # set up the qtt namespace
 # flake8: noqa (we don't need the "<...> imported but unused" error)
 
-import numpy as np
-
 import qtpy
 import matplotlib
 try:
@@ -13,9 +11,23 @@ except:
     pass
 import qtt.live
 import qtt.tools
+
+# todo: remove import *
 from qtt.tools import *
 from qtt.data import *
-
 from qtt.algorithms import *
 
 from qcodes.plots.pyqtgraph import QtPlot
+
+#%% Enhance the qcodes functionality
+
+from qtpy.QtCore import Qt
+
+def keyPressEvent(self, e):
+    ''' Patch to add a callback to the QtPlot figure window '''
+    if e.key() == Qt.Key_P:
+        print('key P pressed: copy figure window to powerpoint')
+        qtt.tools.addPPTslide(txt='', fig=self)
+    super(QtPlot, self).keyPressEvent(e)
+        
+QtPlot.keyPressEvent=keyPressEvent # update the keypress callback function
