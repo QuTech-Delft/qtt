9d99f2bac6470bb76698babeac768b3df9f706f4
CJvanDiepen <c.j.vandiepen-1@tudelft.nl>
2016-09-05 15:44:01 +0200

    Add sweep_init and sweep_run to virtual_awg class to sweep gates with AWGs

--- qtt/instrument_drivers/virtual_awg.py
+++ qtt/instrument_drivers/virtual_awg.py
@@ -13,36 +13,29 @@ Created on Wed Aug 31 13:04:09 2016
 
 #%%
 import numpy as np
-import qcodes
 from qcodes import Instrument
-import scipy
-import logging
 
 #%%
 class virtual_awg(Instrument):
-    shared_kwargs = ['instruments', 'hardware']
-    
     def __init__(self, name, instruments, awg_map, hardware, verbose=1, **kwargs):
+#        shared_kwargs = ['instruments']
         super().__init__(name, **kwargs)
         self._awgs=instruments
         self.awg_map = awg_map
         self.hardware = hardware
         self.verbose = verbose
-        qcodes.installZMQlogger()
         
-        logging.info('virtual_awg: setup')
-        if 'awg_mk' in self.awg_map.keys():
-            awg_cont = self._awgs[self.awg_map['awg_mk'][0]]
-            awg_cont.set('run_mode','CONT')
-            for awg_seq in self._awgs:
-                if awg_seq != awg_cont:
-                    awg_seq.set('run_mode','SEQ')
-                    awg_seq.set_sq_length(1)
+        if len(self._awgs) == 1:
+            self.awg_cont = self._awgs[0]
+            self.awg_cont.set('run_mode','CONT')
+        elif len(self._awgs == 2) and 'awg_mk' in self.awg_map.keys():
+            self.awg_cont = self._awgs[self.awg_map['awg_mk'][0]]
+            self.awg_cont.set('run_mode','CONT')
+            self.awg_seq = self._awgs[(self.awg_map['awg_mk'[0]]+1) % 2]
+            self.awg_seq.set('run_mode','SEQ')
+            self.awg_seq.set_sq_length(1)
         else:
-            for awg in self._awgs:
-                awg.set('run_mode','CONT')
-            if self.verbose:
-                print('All AWGs are set in continuous mode')
+            raise Exception('Configuration of AWGs not supported by virtual_awg instrument')
 
         self.AWG_clock = 1e7
         ch_amp = 4.0
@@ -51,7 +44,6 @@ class virtual_awg(Instrument):
             awg.delete_all_waveforms_from_list()
             for i in range(1,5):
                 awg.set('ch%s_amp'% i, ch_amp)
-        logging.info('virtual_awg: done')
         
     def get_idn(self):
         ''' Overrule because the default VISA command does not work '''
@@ -69,49 +61,91 @@ class virtual_awg(Instrument):
             awg.set('ch4_state',0)
         if verbose:
             print('Stopped AWGs')
-            
-    def sweep_gate(self, gate, sweeprange, risetime):
-        '''Send a triangular signal with the AWG to a gate to sweep. Also 
-        send a marker to the FPGA.
+        
+    def sweep_init(self, waveforms):
+        ''' Send waveform(s) to gate(s)
         
         Arguments:
-            gate (string): the name of the gate to sweep
-            sweeprange (float): the range of voltages to sweep over
-            risetime (float): the risetime of the triangular signal
+            waveforms (dict): the waveforms with the gates as keys
         
         Returns:
-            wave (array): The wave being send with the AWG.
+            sweep_info (dict): the keys are tuples of the awgs and channels to activate
         
         Example:
         -------
-        >>> wave = sweep_gate('P1',sweeprange=60,risetime=1e-3)
+        >>> sweep_info = sweep_init(waveforms) 
         '''
-        awg=self._awgs[self.awg_map[gate][0]]
-        wave_ch=self.awg_map[gate][1]
-        awg_fpga=self._awgs[self.awg_map['fpga_mk'][0]]
-        fpga_ch=self.awg_map['fpga_mk'][1]
-        fpga_ch_mk=self.awg_map['fpga_mk'][2]
+        sweepgates = [g[1] for g in waveforms]
         
-        tri_wave='tri_wave'
-        awg_to_plunger = self.hardware.parameters['awg_to_%s' % gate].get()
-        v_wave=float(sweeprange/((awg.get('ch%d_amp' % wave_ch)/2.0)))
-        v_wave=v_wave/awg_to_plunger
-        samplerate = 1./self.AWG_clock
-        tt = np.arange(0,2*risetime+samplerate,samplerate)
-        wave=(v_wave/2)*scipy.signal.sawtooth(np.pi*tt/risetime, width=.5)
-        awg.send_waveform_to_list(wave,np.zeros(len(wave)),np.zeros(len(wave)),tri_wave)
+        for awg in self._awgs:
+            awg.delete_all_waveforms_from_list()
+            
+        awgs = [self._awgs[self.awg_map[g][0]] for g in sweepgates]
+        fpga_info = self.awg_map['fpga_mk']
+        awgs.append(self._awgs[fpga_info[0]])
         
-        delay_FPGA=25e-6
-        marker2 = np.zeros(len(wave))
-        marker2[int(delay_FPGA/samplerate):int((delay_FPGA+0.40*risetime)/samplerate)]=1.0
-        fpga_marker='fpga_marker'
-        awg_fpga.send_waveform_to_list(np.zeros(len(wave)),np.zeros(len(wave)),marker2,fpga_marker)
+        sweep_info = dict()
+        wave_zero = np.zeros(len(waveforms[sweepgates[0]]))
+        for g in sweepgates:
+            sweep_info[self.awg_map[g]] = dict()
+            sweep_info[self.awg_map[g]]['waveform'] = waveforms[g]
+            sweep_info[self.awg_map[g]]['marker1'] = wave_zero
+            sweep_info[self.awg_map[g]]['marker2'] = wave_zero
+            sweep_info[self.awg_map[g]]['name'] = 'waveform_%s' % g
         
-        awg.set('ch%i_waveform' %wave_ch,tri_wave)
-        awg.set('ch%i_state' %wave_ch,1)
-        awg_fpga.set('ch%i_waveform' %fpga_ch,fpga_marker)
-        awg_fpga.set('ch%i_m%i_low' % (fpga_ch, fpga_ch_mk),0)
-        awg_fpga.set('ch%i_m%i_high' % (fpga_ch, fpga_ch_mk),2.6)
-        awg_fpga.set('ch%i_state' %fpga_ch,1)
-        awg.run()
-        awg_fpga.run()
+        # fpga marker
+        delay_FPGA = 25.0e-6 # should depend on filterboxes
+        fpga_marker = wave_zero
+        fpga_marker[int(delay_FPGA*self.AWG_clock):(int(delay_FPGA*self.AWG_clock)+len(wave_zero)//20)]=1.0
+        
+        if fpga_info[:2] not in sweep_info:
+            sweep_info[fpga_info[:2]] = dict()
+            sweep_info[fpga_info[:2]]['waveform'] = wave_zero
+            sweep_info[fpga_info[:2]]['marker1'] = wave_zero
+            sweep_info[fpga_info[:2]]['marker2'] = wave_zero
+            sweep_info[fpga_info[:2]]['name'] = 'fpga_mk'
+            
+        sweep_info[fpga_info[:2]]['marker%d' % fpga_info[2]] = fpga_marker
+        self._awgs[fpga_info[0]].set('ch%i_m%i_low' % (fpga_info[1], fpga_info[2]),0)
+        self._awgs[fpga_info[0]].set('ch%i_m%i_low' % (fpga_info[1], fpga_info[2]),2.6)
+        
+        # awg marker
+        if self.awg_seq in awgs:
+            awg_info = self.awg_map['awg_mk']
+            if awg_info[:2] not in sweep_info:
+                awgs.append(self._awgs[awg_info[0]])
+                sweep_info[awg_info[:2]] = dict()
+                sweep_info[awg_info[:2]]['waveform'] = wave_zero
+                sweep_info[awg_info[:2]]['marker1'] = wave_zero
+                sweep_info[awg_info[:2]]['marker2'] = wave_zero
+                sweep_info[awg_info[:2]]['name'] = 'awg_mk'
+            
+            delay_AWG=22.0e-6
+            awg_marker = wave_zero
+            awg_marker[0:len(wave_zero)//20]=1
+            awg_marker=np.roll(awg_marker,len(wave_zero)-int(delay_AWG*self.AWG_clock))
+            sweep_info[awg_info[:2]]['marker%d' % self.awg_map['awg_mk'][2]] = awg_marker
+            self._awgs[awg_info[0]].set('ch%i_m%i_low' % (awg_info[1], awg_info[2]),0)
+            self._awgs[awg_info[0]].set('ch%i_m%i_low' % (awg_info[1], awg_info[2]),2.6)
+        
+        # send waveforms
+        for sweep in sweep_info:
+            if self._awgs[sweep[0]] == self.awg_seq:
+                self._awgs[sweep[0]].set_sqel_waveform(sweep_info[sweep]['name'],sweep[1],1)
+                self._awgs[sweep[0]].set_sqel_loopcnt_to_inf(1)
+                self._awgs[sweep[0]].set_sqel_event_jump_target_index(sweep[1],1)
+                self._awgs[sweep[0]].set_sqel_event_jump_type(1,'IND')
+            else:
+                self._awgs[sweep[0]].send_waveform_to_list(sweep_info[sweep]['waveform'],sweep_info[sweep]['marker1'],sweep_info[sweep]['marker2'],sweep_info['name'])
+                self._awgs[sweep[0]].set('ch%i_waveform' % sweep[1], sweep_info[sweep]['name'])
+        
+        return sweep_info
+        
+    def sweep_run(self,sweep_info):
+        ''' Activate AWG(s) and channel(s) for the sweep(s) '''
+        for sweep in sweep_info:
+            sweep[0].set('ch%i_state' %sweep[1],1)
+            
+        awgs = [sweep[0] for sweep in sweep_info]
+        for awg in np.unique(awgs):
+            awg.run()
