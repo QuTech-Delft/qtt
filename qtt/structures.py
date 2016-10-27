661d8ec225a5ff7a1c25d6f857b557a168a95631
CJvanDiepen <c.j.vandiepen-1@tudelft.nl>
2016-10-27 11:52:31 +0200

    Fix instrument get in __repr__ in sensingdot object

--- qtt/structures.py
+++ qtt/structures.py
@@ -33,10 +33,13 @@ class sensingdot_t:
                 'keithley%d' % index].amplitude.get
 
     def __repr__(self):
+        s = 'sensingdot_t'
+        return s
+
+    def show(self):
         gates = self.station.gates
         s = 'sensingdot_t: %s: %s: g %.1f, value %.1f/%.1f' % (
             self.gg[1], str(self.sdval), gates.get(self.gg[1]), self.value(), self.targetvalue)
-        # s='sensingdot_t: %s: %.1f '  % (self.gg[1], self.value() )
         return s
 
     def initialize(self, sdval=None, setPlunger=False):
