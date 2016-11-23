a95ba4e575c0b75f96ab2db71f15c4a15d775026
Pieter <pieter.eendebak@gmail.com>
2016-11-23 23:01:24 +0100

    add FermiLinear function

---
+++ qtt/algorithms/functions.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def FermiLinear(x, a, b, cc, A, T, kb=1):
+    """ Fermi distribution with linear function added 
+    
+    Arguments:
+        x (numpy array): independent variable
+        a, b (float): coefficients of linear part
+        cc (float): center of Fermi distribution
+        A (float): amplitude of Fermi distribution
+        T (float): temperature Fermi distribution
+        kb (float, default: 1): temperature scaling factor
+
+    Returns:
+        y (numpy array): value of the function
+        
+    .. math::
+        
+        y = a*x + b + A*(1/ (1+\exp( (x-cc)/(kb*T) ) ) )
+        
+
+    """        
+    y = a*x+b+A*1./(1+np.exp( (x-cc)/(kb*T) ) )
+    return y
+
