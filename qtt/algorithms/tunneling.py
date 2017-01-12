579ce14763385847d64291238a54e4e356909d45
peendebak <P.T.eendebak@tudelft.nl>
2017-01-12 16:00:56 +0100

    tunneling fit (#51)
    * tunneling
    
    * Pep8
    
    * Fix the usage of maxiter and maxfun in fit_pol_all for the tunneling coupling calculation from a polarization line scan.
    
    * fix merge issue

--- qtt/algorithms/tunneling.py
+++ qtt/algorithms/tunneling.py
@@ -55,8 +55,10 @@ def polweight_all_2slopes(delta, data, par, kT=0.001):
     return total
 
 
-def fit_pol_all(delta, data, kT=0.001, verbose=1):
-    ''' Calculate initial values for fitting and fit
+def fit_pol_all(delta, data, kT=0.001, maxiter=None, maxfun=5000, verbose=1, par_guess=None):
+    ''' Calculate initial values for fitting and fit. The default value for the 
+    maxiter argument of scipy.optimize.fmin is N*200 the number of variables, 
+    i.e. 1200 in our case.
 
     Arguments:
         delta (array):
@@ -64,24 +66,25 @@ def fit_pol_all(delta, data, kT=0.001, verbose=1):
     Returns:
         par_fit (array):
     '''
-    slope_guess = np.polyfit(delta[0:99], data[0:99], 1)[0]  # hard-coded 30 points
-#    slope_guess = np.mean(np.diff(data[0:99]))/np.mean(np.diff(delta[0:99])) # hard-coded 30 points
-    dat_noslope = data - slope_guess * delta
-    b = np.round(len(data) / 2)
-#    sensor_offset_guess = data[int(b)-1]
-    sensor_offset_guess = np.mean(data)
-#    delta_offset_guess = np.mean(delta) # average of the delta values
-    delta_offset_guess = delta[int(b) - 1]
-    sensitivity_guess = np.max(dat_noslope) - np.min(dat_noslope)
-    sensor_offset_guess = sensor_offset_guess - .5 * sensitivity_guess
+    if par_guess is None:
+        slope_guess = np.polyfit(delta[0:99], data[0:99], 1)[0]  # hard-coded 30 points
+    #    slope_guess = np.mean(np.diff(data[0:99]))/np.mean(np.diff(delta[0:99])) # hard-coded 30 points
+        dat_noslope = data - slope_guess * delta
+        b = np.round(len(data) / 2)
+    #    sensor_offset_guess = data[int(b)-1]
+        sensor_offset_guess = np.mean(data)
+    #    delta_offset_guess = np.mean(delta) # average of the delta values
+        delta_offset_guess = delta[int(b) - 1]
+        sensitivity_guess = np.max(dat_noslope) - np.min(dat_noslope)
+        sensor_offset_guess = sensor_offset_guess - .5 * sensitivity_guess
 
-    t_guess = 1 * 4.2 / 80
-#    t_guess = 1 # hard-coded value
-    par_guess = np.array([t_guess, delta_offset_guess, sensor_offset_guess, slope_guess, slope_guess, sensitivity_guess])  # second slope guess could be done more accurately
+        t_guess = 1 * 4.2 / 80
+    #    t_guess = 1 # hard-coded value
+        par_guess = np.array([t_guess, delta_offset_guess, sensor_offset_guess, slope_guess, slope_guess, sensitivity_guess])  # second slope guess could be done more accurately
 
     func = lambda par: polweight_all_2slopes(delta, data, par, kT=kT)
 
-    par_fit = scipy.optimize.fmin(func, par_guess, maxiter=200, maxfun=5000, disp=verbose >= 2)
+    par_fit = scipy.optimize.fmin(func, par_guess, maxiter=maxiter, maxfun=maxfun, disp=verbose >= 2)
 
     return par_fit, par_guess
 
@@ -125,9 +128,9 @@ if __name__ == '__main__':
 
     pp = np.zeros((len(noise), 6))
     for ii, n in enumerate(noise):
-        print('fit %d' % ii)
+        pgeometry.tprint('quick fit %d/%d' % (ii, len(noise)))
         yyx = yy + n * (np.random.rand(yy.size) - .5)
-        parfit, _ = fit_pol_all(xx, yyx)
+        parfit, _ = fit_pol_all(xx, yyx, par_guess=None)
         pp[ii] = parfit
 
     plt.figure(200)
@@ -137,6 +140,31 @@ if __name__ == '__main__':
     plt.ylabel('Estimated tunnel frequency')
 
     pgeometry.plot2Dline([0, -1, par[0]], '--c', label='true value')
+
+    #%% Show effect of proper initialization
+    yyx = yy + n * (np.random.rand(yy.size) - .5)
+    parfit1, _ = fit_pol_all(xx, yyx, par_guess=par)
+    parfit2, _ = fit_pol_all(xx, yyx, par_guess=None, verbose=2)
+    parfit2i, _ = fit_pol_all(xx, yyx, par_guess=parfit2)
+
+    yy1 = polmod_all_2slopes(xx, parfit1, kT=0.001)
+    yy2 = polmod_all_2slopes(xx, parfit2, kT=0.001)
+    yy2i = polmod_all_2slopes(xx, parfit2i, kT=0.001)
+
+    c0 = polweight_all_2slopes(xx, yy, par, kT=0.001)
+    c1 = polweight_all_2slopes(xx, yy1, par, kT=0.001)
+    c2 = polweight_all_2slopes(xx, yy2, par, kT=0.001)
+    c2i = polweight_all_2slopes(xx, yy2i, par, kT=0.001)
+    print('cost %f init %f guess %f -> %f' % (c0, c1, c2, c2i))
+
+    plt.figure(300)
+    plt.clf()
+    plt.plot(xx, yy, '-', label='model')
+    plt.plot(xx, yyx, '.b', label='data')
+    plt.plot(xx, yy1, '--r', label='fitted with gt')
+    plt.plot(xx, yy2, '--g', label='fitted with guess')
+    plt.legend()
+    _ = plt.ylabel('Frequency')
 
     #%% Full estimate
     niter = 60
