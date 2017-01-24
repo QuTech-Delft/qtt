b6ce472109260be46e6ddb13557a36d864f772b8
Pieter <pieter.eendebak@gmail.com>
2017-01-24 17:47:44 +0100

    fix for new scipy

--- qtt/algorithms/onedot.py
+++ qtt/algorithms/onedot.py
@@ -279,7 +279,7 @@ def onedotGetBalance(od, dd, verbose=1, fig=None, drawpoly=False, polylinewidth=
     if distutils.version.LooseVersion(scipy.version.version) < '0.18':
         opts = dict({'disp': verbose >= 2, 'ftol': 1e-6, 'xtol': 1e-5})
     else:
-        opts = dict({'disp': verbose >= 2, 'fatol': 1e-6, 'xtol': 1e-5})
+        opts = dict({'disp': verbose >= 2, 'fatol': 1e-6, 'xatol': 1e-5})
 
     xx = scipy.optimize.minimize(ff, x0, method='Nelder-Mead', options=opts)
     # print('  optimize: %f->%f' % (ff(x0), ff(xx.x)) )
