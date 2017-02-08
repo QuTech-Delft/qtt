9f32c49f34e455a4c1562953addbe33612a3f2ac
CJvanDiepen <c.j.vandiepen-1@tudelft.nl>
2017-02-08 11:49:37 +0100

    Pep8

--- qtt/algorithms/tunneling.py
+++ qtt/algorithms/tunneling.py
@@ -51,7 +51,7 @@ def polweight_all_2slopes(delta, data, par, kT):
             - par[4]: slope of sensor signal on right side
             - par[5]: height of transition, i.e. sensitivity for electron transition
         kT (float): temperature in ueV
-        
+
     Returns:
         total (float): sum of residues
     """
@@ -65,7 +65,7 @@ def polweight_all_2slopes(delta, data, par, kT):
 
 def fit_pol_all(delta, data, kT, maxiter=None, maxfun=5000, verbose=1, par_guess=None):
     """ Polarization line fitting. 
-    
+
     The default value for the maxiter argument of scipy.optimize.fmin is N*200
     the number of variables, i.e. 1200 in our case.
 
@@ -73,7 +73,7 @@ def fit_pol_all(delta, data, kT, maxiter=None, maxfun=5000, verbose=1, par_guess
         delta (1 x N array): chemical potential difference in ueV
         data (1 x N array): sensor data, e.g. from a sensing dot or QPC
         kT (float): temperature in ueV
-        
+
     Returns:
         par_fit (1 x 6 array): fitted parameters
             - par[0]: tunnel coupling in ueV
@@ -85,13 +85,13 @@ def fit_pol_all(delta, data, kT, maxiter=None, maxfun=5000, verbose=1, par_guess
         par_guess (1 x 6 array): initial guess of parameters for fitting
     """
     if par_guess is None:
-        t_guess = 20 # hard-coded guess in ueV
-        numpts = round(len(delta)/10)
+        t_guess = 20  # hard-coded guess in ueV
+        numpts = round(len(delta) / 10)
         slope_guess = np.polyfit(delta[-numpts:], data[-numpts:], 1)[0]
-        dat_noslope = data - slope_guess * (delta-delta[0])
+        dat_noslope = data - slope_guess * (delta - delta[0])
         dat_noslope_1der = scipy.ndimage.filters.gaussian_filter(dat_noslope, sigma=20, order=1)
         trans_idx = np.abs(dat_noslope_1der).argmax()
-        sensitivity_guess = np.sign(delta[-1]-delta[0])*np.sign(dat_noslope_1der[trans_idx])*(np.percentile(dat_noslope, 90) - np.percentile(dat_noslope, 10))
+        sensitivity_guess = np.sign(delta[-1] - delta[0]) * np.sign(dat_noslope_1der[trans_idx]) * (np.percentile(dat_noslope, 90) - np.percentile(dat_noslope, 10))
         delta_offset_guess = delta[trans_idx]
         sensor_offset_guess = data[trans_idx] - sensitivity_guess / 2
         par_guess = np.array([t_guess, delta_offset_guess, sensor_offset_guess, slope_guess, slope_guess, sensitivity_guess])
@@ -112,13 +112,13 @@ if __name__ == '__main__':
     # generate data
     par = np.array([.2, 0, .1, -.0031, -.001, .21])
     #xx0 = np.arange(-10, 10, .1)
-    xx0=np.linspace(-10, 10, 200)
+    xx0 = np.linspace(-10, 10, 200)
     #xx0=4*np.linspace(-10, 10, 200)
-    xx=xx0
-    xx=2*xx0
-    #xx=xx0
+    xx = xx0
+    xx = 2 * xx0
+    # xx=xx0
     yy0 = polmod_all_2slopes(xx0, par, kT=0.001)
-    yy=yy0 + .015 * (np.random.rand(yy0.size) - .5)
+    yy = yy0 + .015 * (np.random.rand(yy0.size) - .5)
     delta = xx
     data = yy
 
@@ -137,7 +137,7 @@ if __name__ == '__main__':
     plt.xlabel('x')
     plt.ylabel('value')
     plt.plot(xx, yyfit, '-r', label='fitted')
-    plt.title('fitted t %.3f, gt %.3f'  % (parfit[0], par[0]))
+    plt.title('fitted t %.3f, gt %.3f' % (parfit[0], par[0]))
     plt.legend(numpoints=1)
 
     #%% Quick estimate
