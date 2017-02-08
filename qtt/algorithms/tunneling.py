3c034269d76fa028283e0df89167404422e75f47
CJvanDiepen <c.j.vandiepen-1@tudelft.nl>
2017-02-08 11:33:30 +0100

    Add documentation and fixes for polarization line fitting

--- qtt/algorithms/tunneling.py
+++ qtt/algorithms/tunneling.py
@@ -8,46 +8,54 @@ Created on Fri Nov 11 17:11:56 2016
 #%%
 import scipy.optimize
 import numpy as np
+import scipy.ndimage
 
 
-def polmod_all_2slopes(x, par, kT=0.001):
-    ''' Polarization line model
+def polmod_all_2slopes(delta, par, kT):
+    """ Polarization line model.
 
-    Arguments:
-        x (1 x N array): input values (e.g. gate voltages)
+    Args:
+        delta (1 x N array): chemical potential difference in ueV
         par (1 x 6 array): parameters for the model
-            - par[0]: tunnel coupling in ...
-            - par[1]: average x value
-            - par[2]: background signal
-            - par[3]: slope on left side
-            - par[4]: slope on right side
-            - par[5]: sensitivity
-        kt (float): temperature in mV
+            - par[0]: tunnel coupling in ueV
+            - par[1]: offset in delta for center of transition
+            - par[2]: offset in background signal
+            - par[3]: slope of sensor signal on left side
+            - par[4]: slope of sensor signal on right side
+            - par[5]: height of transition, i.e. sensitivity for electron transition
+        kT (float): temperature in ueV
 
     Returns:
-        E (float): read-out value of the charge sensor (e.g. using the sensing dot)
-    '''
-    x = x - par[1]
-
-    Om = np.sqrt(x**2 + 4 * par[0]**2)
-    E = 1 / 2 * (1 + x / Om * np.tanh(Om / (2 * kT)))
+        E (float): sensor data, e.g. from a sensing dot or QPC
+    """
+    delta = delta - par[1]
+    Om = np.sqrt(delta**2 + 4 * par[0]**2)
+    E = 1 / 2 * (1 + delta / Om * np.tanh(Om / (2 * kT)))
     slopes = par[3] + (par[4] - par[3]) * E
-    E = par[2] + x * slopes + E * par[5]
+    E = par[2] + delta * slopes + E * par[5]
 
     return E
 
 
-def polweight_all_2slopes(delta, data, par, kT=0.001):
-    ''' Cost function for polarization fitting
+def polweight_all_2slopes(delta, data, par, kT):
+    """ Cost function for polarization fitting.
 
-    Arguments:
-        delta (array):
-        data (array):
-        par (array):
+    Args:
+        delta (1 x N array): chemical potential difference in ueV
+        data (1 x N array): sensor data, e.g. from a sensing dot or QPC
+        par (1 x 6 array): parameters for the model
+            - par[0]: tunnel coupling in ueV
+            - par[1]: offset in delta for center of transition
+            - par[2]: offset in background signal
+            - par[3]: slope of sensor signal on left side
+            - par[4]: slope of sensor signal on right side
+            - par[5]: height of transition, i.e. sensitivity for electron transition
+        kT (float): temperature in ueV
+        
     Returns:
-        total (float):
-    '''
-    mod = polmod_all_2slopes(delta, par, kT=kT)
+        total (float): sum of residues
+    """
+    mod = polmod_all_2slopes(delta, par, kT)
     diffs = data - mod
     norms = np.sqrt(np.sum(diffs**2))
     total = np.sum(norms)
@@ -55,35 +63,40 @@ def polweight_all_2slopes(delta, data, par, kT=0.001):
     return total
 
 
-def fit_pol_all(delta, data, kT=0.001, maxiter=None, maxfun=5000, verbose=1, par_guess=None):
-    ''' Calculate initial values for fitting and fit. The default value for the 
-    maxiter argument of scipy.optimize.fmin is N*200 the number of variables, 
-    i.e. 1200 in our case.
+def fit_pol_all(delta, data, kT, maxiter=None, maxfun=5000, verbose=1, par_guess=None):
+    """ Polarization line fitting. 
+    
+    The default value for the maxiter argument of scipy.optimize.fmin is N*200
+    the number of variables, i.e. 1200 in our case.
 
-    Arguments:
-        delta (array):
-        data (array):
+    Args:
+        delta (1 x N array): chemical potential difference in ueV
+        data (1 x N array): sensor data, e.g. from a sensing dot or QPC
+        kT (float): temperature in ueV
+        
     Returns:
-        par_fit (array):
-    '''
+        par_fit (1 x 6 array): fitted parameters
+            - par[0]: tunnel coupling in ueV
+            - par[1]: offset in delta for center of transition
+            - par[2]: offset in background signal
+            - par[3]: slope of sensor signal on left side
+            - par[4]: slope of sensor signal on right side
+            - par[5]: height of transition, i.e. sensitivity for electron transition
+        par_guess (1 x 6 array): initial guess of parameters for fitting
+    """
     if par_guess is None:
-        slope_guess = np.polyfit(delta[0:99], data[0:99], 1)[0]  # hard-coded 30 points
-    #    slope_guess = np.mean(np.diff(data[0:99]))/np.mean(np.diff(delta[0:99])) # hard-coded 30 points
-        dat_noslope = data - slope_guess * delta
-        b = np.round(len(data) / 2)
-    #    sensor_offset_guess = data[int(b)-1]
-        sensor_offset_guess = np.mean(data)
-    #    delta_offset_guess = np.mean(delta) # average of the delta values
-        delta_offset_guess = delta[int(b) - 1]
-        sensitivity_guess = np.max(dat_noslope) - np.min(dat_noslope)
-        sensor_offset_guess = sensor_offset_guess - .5 * sensitivity_guess
+        t_guess = 20 # hard-coded guess in ueV
+        numpts = round(len(delta)/10)
+        slope_guess = np.polyfit(delta[-numpts:], data[-numpts:], 1)[0]
+        dat_noslope = data - slope_guess * (delta-delta[0])
+        dat_noslope_1der = scipy.ndimage.filters.gaussian_filter(dat_noslope, sigma=20, order=1)
+        trans_idx = np.abs(dat_noslope_1der).argmax()
+        sensitivity_guess = np.sign(delta[-1]-delta[0])*np.sign(dat_noslope_1der[trans_idx])*(np.percentile(dat_noslope, 90) - np.percentile(dat_noslope, 10))
+        delta_offset_guess = delta[trans_idx]
+        sensor_offset_guess = data[trans_idx] - sensitivity_guess / 2
+        par_guess = np.array([t_guess, delta_offset_guess, sensor_offset_guess, slope_guess, slope_guess, sensitivity_guess])
 
-        t_guess = 1 * 4.2 / 80
-    #    t_guess = 1 # hard-coded value
-        par_guess = np.array([t_guess, delta_offset_guess, sensor_offset_guess, slope_guess, slope_guess, sensitivity_guess])  # second slope guess could be done more accurately
-
-    func = lambda par: polweight_all_2slopes(delta, data, par, kT=kT)
-
+    func = lambda par: polweight_all_2slopes(delta, data, par, kT)
     par_fit = scipy.optimize.fmin(func, par_guess, maxiter=maxiter, maxfun=maxfun, disp=verbose >= 2)
 
     return par_fit, par_guess
