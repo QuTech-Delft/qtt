2884f579b168def3b6e2a220c52b417492d921b6
CJvanDiepen <c.j.vandiepen-1@tudelft.nl>
2017-02-08 13:07:06 +0100

    Rename eff_gates to virtual_gates and add sweepmap functionality

--- qtt/instrument_drivers/virtual_gates.py
+++ qtt/instrument_drivers/virtual_gates.py
@@ -12,67 +12,78 @@ from qcodes.utils.validators import Numbers
 import numpy as np
 
 
-class eff_gates(Instrument):
-    '''
-    Create effective gates, which are linear combinations of the physical gates.
-    They are defined, such that when changing one of the effective gates, the
-    others are (almost) not influenced. The effective gates are developed in 
-    a way such that they correspond to the chemical potentials in mV's.
+class virtual_gates(Instrument):
+    """ Create virtual gates, which are linear combinations of the gates.
+    
+    The virtual gates are defined, such that when changing one of the virtual 
+    gates, the others are not influenced. The virtual gates can be used for
+    changing only one physical parameter, e.g. a chemical potentials or a 
+    tunnel coupling. Note: They do not (yet?) have an offset relative to the 
+    physical gates.
 
-    They do not (yet?) have an offset relative to the physical gates.
-
-    Input:
-        name (string):
-        gates (object):
-        eff_gate_map ():
-    '''
+    Attributes:
+        name (string): The name of the object
+        gates (Instrument): the physical gates
+        virt_gate_map (dict): describes the transformation matrix
+    """
     shared_kwargs = ['gates']
 
-    def __init__(self, name, gates, eff_gate_map, **kwargs):
+    def __init__(self, name, gates, virt_gate_map, sweepgates=None, **kwargs):
         super().__init__(name, **kwargs)
 
         self.gates = gates
-        self.map = eff_gate_map
+        self.map = virt_gate_map
         self._gates_list = sorted(list(self.map[list(self.map.keys())[0]].keys()))
-        self._eff_gates_list = sorted(list(self.map.keys()))
-        self._matrix = np.array([[self.map[x].get(y, 0) for y in self._gates_list] for x in self._eff_gates_list])
+        self._virt_gates_list = sorted(list(self.map.keys()))
+        self._matrix = np.array([[self.map[x].get(y, 0) for y in self._gates_list] for x in self._virt_gates_list])
         self._matrix_inv = np.linalg.inv(self._matrix)
         self.map_inv = dict()
-        for ideff, effg in enumerate(self._eff_gates_list):
-            self.map_inv[effg] = dict()
+        for idvirt, virtg in enumerate(self._virt_gates_list):
+            self.map_inv[virtg] = dict()
             for idg, g in enumerate(self._gates_list):
-                self.map_inv[effg][g] = self._matrix_inv[idg][ideff]  # swapped idg and ideff
+                self.map_inv[virtg][g] = self._matrix_inv[idg][idvirt]
 
-        for g in self._eff_gates_list:
+        for g in self._virt_gates_list:
             self.add_parameter(g,
                                label='%s (mV)' % g,
                                units='mV',
                                get_cmd=partial(self._get, gate=g),
                                set_cmd=partial(self._set, gate=g),
                                vals=Numbers(-2000, 2000))
+                               
+        if sweepgates == None:
+            self.sweepgates = self._virt_gates_list
+            self.sweepmap = self.map_inv
+        else:
+            self.sweepgates = sweepgates
+            self.sweepmap = dict()
+            for gvirt in self.sweepgates:
+                self.sweepmap[gvirt+'_virt'] = dict()
+                for g in sweepgates:
+                    self.sweepmap[gvirt+'_virt'][g] = self.map_inv[gvirt+'_virt'][g]
 
     def _get(self, gate):
         gateval = sum([self.map[gate][g] * self.gates[g].get() for g in self.map[gate]])
         return gateval
 
     def _set(self, value, gate):
-        gate_vec = np.zeros(len(self._eff_gates_list))
+        gate_vec = np.zeros(len(self._virt_gates_list))
         increment = value - self.get(gate)
-        gate_vec[self._eff_gates_list.index(gate)] = increment
+        gate_vec[self._virt_gates_list.index(gate)] = increment
         set_vec = np.dot(self._matrix_inv, gate_vec)
         for idx, g in enumerate(self._gates_list):
             self.gates.set(g, self.gates.get(g) + set_vec[idx])
         return
 
     def allvalues(self):
-        """ Return all eff_gates values in a simple dict """
-        vals = [(gate, self.get(gate)) for gate in self._eff_gates_list]
+        """ Return all virtual gates values in a dict. """
+        vals = [(gate, self.get(gate)) for gate in self._virt_gates_list]
         return dict(vals)
 
     def resetgates(self, activegates, basevalues=None, verbose=2):
-        """ Reset a set of gates to default values
+        """ Reset a set of gates to default values.
 
-        Arguments:
+        Args:
             activegates (list or dict): gates to reset
             basevalues (dict): new values for the gates
             verbose (int): output level
