6350e472612ea6f55f5e5c722588684fbd8c2537
Pieter Eendebak <pieter.eendebak@gmail.com>
2017-03-09 13:37:56 +0100

    add VirtualMeter exception

--- qtt/qtt_toymodel.py
+++ qtt/qtt_toymodel.py
@@ -389,28 +389,7 @@ class VirtualMeter(Instrument):
             model (object): class that provides a `.get` function
             
         """
-        super().__init__(name, **kwargs)
-        self.model = model
-
-        g = 'amplitude'
-        self.add_parameter(g,
-                           label='%s Current (nA)' % name,
-                           get_cmd=partial(self.get_gate, g),
-                           )
-        self.add_parameter('readnext', get_cmd=partial(self.get, 'amplitude'), label=name)
-
-    def get_gate(self, gate):
-        return self.model.get(self.name + '_' + gate)
-
-    def set_gate(self, gate, value):
-        self.model.set(self.name + '_' + gate, value)
-        return
-
-    def get_idn(self):
-        """ Overrule because the default get_idn yields a warning """
-        IDN = {'vendor': 'QuTech', 'model': self.name,
-               'serial': None, 'firmware': None}
-        return IDN
+        raise Exception('please import from qtt.instrument_drivers.virtual_instruments instead')
     
 #%%
 
