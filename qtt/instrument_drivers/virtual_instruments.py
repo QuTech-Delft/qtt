ffbda4f29e8d3c820156cfca3648c2b9258bd5e5
Andrea Corna <a.corna@tudelft.nl>
2017-03-28 18:51:55 +0200

    Add a virtual source instrument. Useful for voltage/current sources in IVVI rack

--- qtt/instrument_drivers/virtual_instruments.py
+++ qtt/instrument_drivers/virtual_instruments.py
@@ -7,6 +7,7 @@
 #%% Load packages
 
 from functools import partial
+from qcodes.utils.validators import Numbers
 
 import qcodes as qc
 from qcodes import Instrument   # , Parameter, Loop, DataArray
@@ -51,3 +52,38 @@ class VirtualMeter(Instrument):
                'serial': None, 'firmware': None}
         return IDN
     
+class VirtualSource(Instrument):
+    """A virtual instrument with a settable paramater
+    """
+    shared_kwargs = ['gates']
+
+    def __init__(self, name, source, parameterParent, parameterChild=None, gain=1.0, unit=None, **kwargs):
+        """Initialize a virtual gates object.
+
+        Args:
+            name (string): The name of the object
+            source (Instrument): the physical source instrument
+            parameterParent (string): the name of the parameter on the parent instrument
+            parameterChild (string, optional): the name of the parameter on the child instrument. If absent, equal to
+                                                the parent one
+            gain (float, optional): the gain of the virtual parameter. Default equal to one.
+            unit (str, optional): the unit of the virtual parameter. Default equal to the parent parameter.
+        """
+        super().__init__(name, **kwargs)
+        self.name = name
+        self.gain = gain
+
+        if not parameterChild:
+            parameterChild = parameterParent
+        print(parameterChild)
+
+        if not unit:
+            unit = source[parameterParent].unit
+
+        self.add_parameter(parameterChild,
+                           label='%s' % parameterChild,
+                           unit=unit,
+                           get_cmd=lambda: source.get(parameterParent)*self.gain,
+                           set_cmd=lambda val: source.set(parameterParent, val/self.gain),
+                           vals=Numbers())  # TODO: Adjust the validator range based on that of the gates
+
