f76bb1356a243ce68223fbddb50f63763768224c
takafumifujita <t.fujita@tudelft.nl>
2017-06-27 00:45:06 +0200

    changes after feedback pull request

--- qtt/algorithms/virtgates.py
+++ qtt/algorithms/virtgates.py
@@ -16,7 +16,7 @@ np.set_printoptions(precision=2,suppress=True, threshold=100)
 import matplotlib.pyplot as plt
 
 import qtt
-from qtt.algorithms.fitting import detect_peaks
+from users.houckm.detect_peaks import detect_peaks
 
 from qtt.algorithms.generic import smoothImage
 
@@ -25,25 +25,47 @@ import qtt.measurements.videomode
 import qtt.structures
 from qtt.data import makeDataSet1Dplain
 from qtt.measurements.scans import create_vectorscan
+
 from qtt.measurements.ttrace import dummy_read,parse_data
 
-
 #%%
-def scan1D_transitions(tlines, CC,startgv, gg, station, gates, virt_gates ,physical_gates, cc_basis, scanjob,  abort=False, mode='real', single_scan1D='off', iteration=1, wait_time_sweep1D=1.2, Naverage=1000, rang=40, nsteps=200, dGstep=5):
+def scan1D_transitions(tlines, CC, station, virt_gates ,physical_gates, cc_basis, scanjob,  abort=False, mode='real', single_scan1D='off', iteration=1, wait_time_sweep1D=1.2, Naverage=1000, rang=40, nsteps=200, dGstep=5):#TODO:Question: What does the wait_time_sweep1D do? Shouldn't it be a sleep time between the two steps?
+#    How to bring a board qtt.abort_measurements()
     """ Makes 1D sweep scans over different transition lines     
-    Question: What does the wait_time_sweep1D do? Shouldn't it be a sleep time between the two steps?
-    How to brign a board qtt.abort_measurements()"""      
+    Inputs:
+        tlines (dict): containing the points on the different transitionlines chosen to sweep over
+        CC (dict): containing all the scanned data
+        station (station): station containing parameters needed for the scan
+        virt_gates (virtual_gates instrument): virtual gates used in the scans
+        physical_gates (list): containing all the physical gates in the setup
+        cc_basis (list): containing all the virtual gates in the setup
+        scanjob (scanjob_t): information needed to do the scans
+        abort (Bool): parameter to abort #TODO: does not work yet
+        mode (string): if you are using af real or simulated setup
+        single_scan1D (string): if 'on' only 2 1D scans are made
+        iteration (float): the number keeping track to of the updates used to store in CC
+        wait_time_sweep1D (float): wait time before each sweep
+        Naverage (float): strength of the averaging filter
+        rang (float): range of each sweep to scan
+        nsteps (float): number of steps done in the scan
+        dGstep (float): stepsize between two sweep scans
+    Outputs:
+        CC (dict): containing all the data
+       """     
+    gates=station.gates
+    startgv=gates.allvalues()
     step=rang/nsteps
-    for h,l in enumerate(physical_gates,0):
+    for h,l in enumerate(cc_basis,0):
         i=2*h+1
         sweepgate=cc_basis[i]
         CC['iteration %d' %iteration]['sweepgate %s'%cc_basis[i]]={}
         transitionline=cc_basis[i]
+        gg=list(tlines[transitionline].keys())
         startgv[gg[0]]=tlines[transitionline][gg[0]]
         startgv[gg[1]]=tlines[transitionline][gg[1]]
-        if  iteration>1:
-            startgv[physical_gates[i]]=CC['iteration %d'%(iteration-1)]['sweepgate %s'%sweepgate]['stepgate tL']['avg_transitionplace_sweepgate']
-    
+#        if  iteration>1:#TODO: Fix that this auto centering works right
+#            startgv[physical_gates[i]]=CC['iteration %d'%(iteration-1)]['sweepgate %s'%sweepgate]['stepgate tL']['avg_transitionplace_sweepgate']
+#    
         gates.resetgates(startgv, startgv,verbose=0)
         startsweep = virt_gates.get(list(virt_gates.allvalues())[i])-rang/2
         if mode=='real':
@@ -82,15 +104,37 @@ def scan1D_transitions(tlines, CC,startgv, gg, station, gates, virt_gates ,physi
     gates.resetgates(startgv, startgv, verbose=0)
     return(CC)
 #%%
-def ttrace_transitions(CC,startgv, station, gates, virt_gates, cc_basis, normal_gates, biasT_gates, t_RC, t_biasT, ttraces,ttrace,amplitudes,abort=False, single_scan1D='off', iteration=2, dGstep=5):
-    """Makes 1D ttraces over transition lines"""
+def ttrace_transitions(CC,station, virt_gates, cc_basis, normal_gates, biasT_gates, t_RC, t_biasT, ttraces,ttrace,amplitudes,abort=False, single_scan1D='off', iteration=2, dGstep=5):#TODO: can the normal gates, biasT gates, and their wait times be extracted from the station?
+    """Makes 1D ttraces over transition lines
+    Inputs:
+        CC (dict): containing all the scanned data
+        station (station): station containing parameters needed for the scan
+        virt_gates (virtual_gates instrument): virtual gates used in the scans
+        cc_basis (list): containing all the virtual gates in the setup
+        normal_gates (list): gates which don't have a biasT
+        biasT_gates (list): gates which have a biasT
+        t_RC: waiting time for normal gates (on the 3dot as a result of the low pass RC filter time)
+        t_biasT: waiting time for gates with bias T(on the 3dot as a result of the bias T acting as an RC filter for DAC steps)
+        ttraces,ttrace (dict): data on the traces put on the awg
+        amplitudes: amplitudes of the traces
+        abort (Bool): parameter to abort #TODO: does not work yet
+        single_scan1D (string): if 'on' only 2 1D scans are made
+        iteration (float): the number keeping track to of the updates used to store in CC
+        dGstep (float): stepsize between two sweep scans
+    Outputs:
+        CC (dict): containing all the data"""
     start_time=time.clock()
+    gates=station.gates
+    startgv=gates.allvalues()
     default_parameter_array_key='measured1'
-    CC['iteration %d' %iteration]=dict({'sweepgate mu1':dict({}),'sweepgate mu2':dict({}),'sweepgate mu3':dict({})})
+    
+    CC['iteration %d' %iteration]=dict({})
+    for aa,bb in enumerate(cc_basis):
+        if aa%2!=0 :
+            CC['iteration %d' %iteration]['sweepgate %s'%bb]=dict({})           
     
     for  j,k in enumerate(cc_basis,0):
         gates.resetgates(startgv, startgv, verbose=0)
-#        virt_gates.set('mu1',(virt_gates.get('mu1')+50)) #update somewhere else in matrix
         
         virt_gates.set(k,(virt_gates.get(k)-dGstep))
         if k in normal_gates:
@@ -98,16 +142,16 @@ def ttrace_transitions(CC,startgv, station, gates, virt_gates, cc_basis, normal_
         else:
             time.sleep(t_biasT)
         
-        data_raw=dummy_read(station,ttraces[0:1])
-        tt, datax, s1a = parse_data(station,data_raw,ttraces,ttrace)
+        data_raw=dummy_read(station)
+        tt, datax, s1a = parse_data(data_raw,ttraces,ttrace)
             
         virt_gates.set(k,(virt_gates.get(k)+2*dGstep))
         if k in normal_gates:
             time.sleep(t_RC)
         else:
             time.sleep(t_biasT)
-        data_raw=dummy_read(station,ttraces[0:1])
-        tt, datax, s2a = parse_data(station,data_raw,ttraces,ttrace)
+        data_raw=dummy_read(station)
+        tt, datax, s2a = parse_data(data_raw,ttraces,ttrace)
         print(k)   
         
         for h,l in enumerate(cc_basis,0):
@@ -116,7 +160,6 @@ def ttrace_transitions(CC,startgv, station, gates, virt_gates, cc_basis, normal_
             if i==j:
                 pass
             else:
-                
                 rang=amplitudes[h]
                 startsweep=virt_gates.get(sweepgate)-rang/2
                 s1=makeDataSet1Dplain(xname='sweepparam', x=(np.linspace(startsweep, (startsweep+rang), num=np.size(s1a[h],1))),yname=default_parameter_array_key,y=s1a[h][0])
@@ -131,13 +174,28 @@ def ttrace_transitions(CC,startgv, station, gates, virt_gates, cc_basis, normal_
     print("ttraceC time%s" % ttraceC)  
     return(CC)
 #%%
-def cc_from_scan1D(CC,physical_gates, cc_basis, dGstep, datadir_CC, iteration=1,mode='real',mph=0.5,mpd=80, single_scan1D='off',analysis=True,valley=True):
-    """Computes cross-capacitance matrix from 1D transitions scans"""
+def cc_from_scan1D(CC, cc_basis, dGstep, datadir_CC, iteration=1,virt_gates_iter=0,mode='real',mph=0.5,mpd=80, single_scan1D='off',valley=True):
+    """Computes cross-capacitance matrix from 1D transitions scans
+    Inputs:
+        CC (dict): containing all the scanned data
+        cc_basis (list): containing all the virtual gates in the setup
+        dGstep (float): stepsize between two sweep scans
+        datadir_CC (string): place to store the output
+        iteration (float): the number keeping track to of the updates used to store in CC
+        virt_gates_iter (float): iteration on which the used virtual gates were based
+        mode (string): if you are using af real or simulated setup
+        mph (float): relative minimum peak height in the peak detecton
+        mpd (float): amount of data points between allowed peaks in the peak detection
+        single_scan1D (string): if 'on' only 2 1D scans are made
+        valley (Bool): if True valley peaks are detected, if False peaks are detected
+    Outputs:
+        CC (dict): array containing the scanned and analysed data as well
+        """
     default_parameter_array_key='measured1'
     meas_arr_name=CC['iteration %d'%iteration]['sweepgate mu1']['stepgate tL']['sweep1']['scan'].default_parameter_name(default_parameter_array_key)
-    c=np.zeros((len(physical_gates),len(physical_gates)))
+    c=np.zeros((len(cc_basis),len(cc_basis)))
     np.fill_diagonal(a=c,val=1)
-    for h,l in enumerate(physical_gates,0):
+    for h,l in enumerate(cc_basis,0):
         i=2*h+1 
         sweepgate=cc_basis[i]
         if mode=='real':
@@ -169,29 +227,22 @@ def cc_from_scan1D(CC,physical_gates, cc_basis, dGstep, datadir_CC, iteration=1,
                         if mode=='sim':
                             transition=False
                             polarisation=True
-#                        peaks_i=detect_peaks(ds.diff_dir_y,mph=0.1*max(ds.diff_dir_y),mpd=100,show=False, valley=polarisation)#best if 0.8
-                        peaks_i=detect_peaks(ds.diff_dir_y,mph=mph*max(ds.diff_dir_y),mpd=mpd,show=False, valley=valley)#best if 0.8
+                        peaks_i=detect_peaks(ds.diff_dir_y,mph=mph*max(ds.diff_dir_y),mpd=mpd,show=False, valley=valley)
                         peaks_sweep=scan.arrays[setpoints][peaks_i]
                         peaks_a=scan.default_parameter_array(default_parameter_array_key)[peaks_i]
                         peaks_da=ds.diff_dir_y[peaks_i]
                         print(peaks_i,peaks_sweep,peaks_a,peaks_da)
                     CC['iteration %d' %iteration]['sweepgate %s'%sweepgate]['stepgate %s'%k][ss]['analysis']=dict({'ds':ds,'peaks_i':peaks_i,'peaks_sweep':peaks_sweep,'peaks_a':peaks_a,'peaks_da':peaks_da}) 
-#                if peaks_i==None:
-#                    break
-#                
-                peaks_i_1=CC['iteration %d' %iteration]['sweepgate %s'%sweepgate]['stepgate %s'%k]['sweep1']['analysis']['peaks_i']#load
-                peaks_i_2=CC['iteration %d' %iteration]['sweepgate %s'%sweepgate]['stepgate %s'%k]['sweep2']['analysis']['peaks_i']#load
-                scan=CC['iteration %d' %iteration]['sweepgate %s'%sweepgate]['stepgate %s'%k]['sweep2']['scan']#load
+#             
+                peaks_i_1=CC['iteration %d' %iteration]['sweepgate %s'%sweepgate]['stepgate %s'%k]['sweep1']['analysis']['peaks_i']
+                peaks_i_2=CC['iteration %d' %iteration]['sweepgate %s'%sweepgate]['stepgate %s'%k]['sweep2']['analysis']['peaks_i']
+                scan=CC['iteration %d' %iteration]['sweepgate %s'%sweepgate]['stepgate %s'%k]['sweep2']['scan']
                 
-                if j==0:            
-                    transition_exp=len(scan.arrays[setpoints])/2
-                else:
-                    transition_exp=CC['iteration %d' %iteration]['sweepgate %s'%sweepgate]['stepgate %s'%cc_basis[0]][ss]['analysis']['for_cc']['peaks_i']
+                transition_exp=len(scan.arrays[setpoints])/2
                 peaks_cc_i_1=peaks_i_1.flat[np.abs(peaks_i_1 - transition_exp).argmin()]
 
                 
-                if iteration==0 or iteration==1:#gets for the second point nearest peak
-#                    peaks_cc_i_2=peaks_i_2.flat[(peaks_i_2 - peaks_cc_i_1).argmin()]
+                if virt_gates_iter==0:#gets for the second point nearest peak
                      peaks_cc_i_2=peaks_i_2[numpy.argmax([n for n in (peaks_i_2 - peaks_cc_i_1) if n<0])]
                 else:
                     peaks_cc_i_2=peaks_i_2.flat[np.abs(peaks_i_2 - peaks_cc_i_1).argmin()]
@@ -221,15 +272,25 @@ def cc_from_scan1D(CC,physical_gates, cc_basis, dGstep, datadir_CC, iteration=1,
         if h==(len(cc_basis)-1)/2-1 or single_scan1D=='on': 
             break
     CC['iteration %d' %iteration]['cc_corr']=c
-    CC['iteration %d' %iteration]['cc']=c@CC['iteration %d' %(1)]['cc']
+    CC['iteration %d' %iteration]['cc']=c@CC['iteration %d' %(virt_gates_iter)]['cc']
     print(CC['iteration %d' %iteration]['cc'])
     
     s=strftime('%d_%m_%H%M', gmtime())
     qtt.write_data(datadir_CC%s,CC)
     return(CC)
+
 #%%
-def plot_scan1D_transitions(CC,physical_gates, cc_basis, title_add=None, iteration=1,mode='real',single_scan1D='off'):
-    """Plots the 1D transitions scans and analysis"""
+def plot_scan1D_transitions(CC, cc_basis, title_add=None, iteration=1,mode='real',single_scan1D='off', analyses=True):
+    """Plots the 1D transitions scans and analysis
+    Input:
+        CC (dict): containing all the scanned data
+        cc_basis (list): containing all the virtual gates in the setup
+        title_add (string): addition on the plots title to make
+        iteration (float): the number keeping track to of the updates used to store in CC
+        mode (string): if you are using af real or simulated setup
+        single_scan1D (string): if 'on' only 2 1D scans are made
+        analyses (Bool): plot the analyses yes or no
+        """
     default_parameter_array_key='measured1'
     if single_scan1D=='on':
         fig, axarr = plt.subplots(1,1)
@@ -237,14 +298,14 @@ def plot_scan1D_transitions(CC,physical_gates, cc_basis, title_add=None, iterati
     else:
         fig, axarr = plt.subplots(int((len(cc_basis)-1)/2),int(len(cc_basis)))
         fig.suptitle('Differentiated transition step for %s' %title_add)
-    if 'analysis' in CC['iteration %d'%iteration]['sweepgate mu1']['stepgate tL']['sweep1']:
+    if analyses==True:
         if single_scan1D=='on':
             figd, axarrd = plt.subplots(1,1)
             figd.suptitle('Transition step')
         else:
             figd, axarrd =  plt.subplots(int((len(cc_basis)-1)/2),int(len(cc_basis)))
             figd.suptitle('Transition step')
-    for h,l in enumerate(physical_gates,0):
+    for h,l in enumerate(cc_basis,0):
         i=2*h+1
         sweepgate=cc_basis[i]
         if mode=='real':
@@ -261,9 +322,9 @@ def plot_scan1D_transitions(CC,physical_gates, cc_basis, title_add=None, iterati
                         axarr.set_title(k)
                         axarr.plot(CCpart[ss]['scan'].arrays[setpoints],CCpart[ss]['scan'].default_parameter_array(default_parameter_array_key)) 
                     else:
-                        axarr[h,j].set_title(k)
+                        axarr[0,j].set_title(k)
                         axarr[h,j].plot(CCpart[ss]['scan'].arrays[setpoints],CCpart[ss]['scan'].default_parameter_array(default_parameter_array_key)) 
-                    if 'analysis' in CC['iteration %d'%iteration]['sweepgate mu1']['stepgate tL']['sweep1']:                       
+                    if analyses==True:                       
                         if single_scan1D=='on':
                             axarr.plot(CCpart[ss]['analysis']['for_cc']['peaks_sweep'],CCpart[ss]['analysis']['for_cc']['peaks_a'],'^m' , markersize=7)
                                                     
@@ -275,7 +336,7 @@ def plot_scan1D_transitions(CC,physical_gates, cc_basis, title_add=None, iterati
                         else:
                             axarr[h,j].plot(CCpart[ss]['analysis']['for_cc']['peaks_sweep'],CCpart[ss]['analysis']['for_cc']['peaks_a'],'^m' , markersize=7)
                                                     
-                            axarrd[h,j].set_title(k)
+                            axarrd[0,j].set_title(k)
                             axarrd[h,j].plot(CCpart[ss]['analysis']['ds'].arrays[setpoints],CCpart[ss]['analysis']['ds'].diff_dir_y)
                             axarrd[h,j].plot(CCpart[ss]['analysis']['ds'].arrays[setpoints],CCpart[ss]['analysis']['ds'].diff_dir_y)
                             axarrd[h,j].plot(CCpart[ss]['analysis']['peaks_sweep'],CCpart[ss]['analysis']['peaks_da'],'^m' , markersize=7)
