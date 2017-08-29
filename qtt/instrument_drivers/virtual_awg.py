126630630911a7d223ea68b8c46431ca15924e75
jpdehollain <j.p.dehollainlorenzana@tudelft.nl>
2017-08-29 13:40:59 +0200

    new function added

--- qtt/instrument_drivers/virtual_awg.py
+++ qtt/instrument_drivers/virtual_awg.py
@@ -531,6 +531,19 @@ class virtual_awg(Instrument):
                 val=f()
                 if val!=4.0:
                     warnings.warn('AWG channel %d output not at 4.0 V' % ii)
+                    
+    def setamplitude(self, amplitude):
+        """ Set the AWG peak-to-peak amplitude for all channels """
+        if amplitude < 0.02:
+            warnings.warn('Trying to set AWG amplitude too low, setting it to minimum (20mV)')
+            amplitude = 0.02
+        elif amplitude > 4.5:
+            warnings.warn('Trying to set AWG amplitude too high, setting it to maximum (4.5V)')
+            amplitude = 4.5
+    
+        self.ch_amp = round(amplitude,3)
+        for i in range(1, 5):
+            self.set('ch%s_amp' % i, self.ch_amp)
 
 #%%
 def plot_wave_raw(wave_raw, samplerate=None, station=None):
