ea7420830a645fa233de3fdce964f0fd894b3e3f
Pieter Eendebak <pieter.eendebak@gmail.com>
2017-09-08 21:31:02 +0200

    improve docs for fermi fitting

--- qtt/algorithms/functions.py
+++ qtt/algorithms/functions.py
@@ -1,6 +1,8 @@
 """ Mathematical functions and models """
 
 import numpy as np
+import scipy.constants
+
 
 def gaussian(x, mean, s, amplitude = 1, offset = 0):
     """ Model for Gaussuan function
@@ -58,27 +60,32 @@ def Fermi(x, cc, A, T, kb=1):
     return y
 
 
-def FermiLinear(x, a, b, cc, A, T, kb=1):
+def FermiLinear(x, a, b, cc, A, T, l = 1.16):
     """ Fermi distribution with linear function added 
 
     Arguments:
-        x (numpy array): independent variable
+        x (numpy array): independent variable 
         a, b (float): coefficients of linear part
         cc (float): center of Fermi distribution
         A (float): amplitude of Fermi distribution
-        T (float): temperature Fermi distribution
-        kb (float, default: 1): temperature scaling factor
+        T (float): temperature Fermi distribution in mili Kelvin
+        l (float): leverarm divided by kb
 
+    The default value of the leverarm is
+        (100 ueV/mV)/kb = (100*1e-6*scipy.constants.eV )/kb = 1.16.
+        
+    For this value the input variable x should be in mV, the 
+    temperature T in K. We input the leverarm divided by kb for numerical stability.
+    
     Returns:
         y (numpy array): value of the function
 
     .. math::
 
-        y = a*x + b + A*(1/ (1+\exp( (x-cc)/(kb*T) ) ) )
-
+        y = a*x + b + A*(1/ (1+\exp( l* (x-cc)/(T) ) ) )
 
     """
-    y = a * x + b + A * 1. / (1 + np.exp((x - cc) / (kb * T)))
+    y = a * x + b + A * 1. / (1 + np.exp(l*(x - cc) / (T)))
     return y
 
 
