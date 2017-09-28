a1c9de805f20f9fd4212292f5b932f78979d3d5b
CJvanDiepen <c.j.vandiepen@tudelft.net>
2017-09-28 10:34:19 +0200

    Clean-up code for acfit_to_ccmap.

--- qtt/algorithms/anticross_fitting.py
+++ qtt/algorithms/anticross_fitting.py
@@ -38,7 +38,7 @@ def acfit_to_ccmap(ac_fits):
         cc_map['V'+plg] = {}
         # first the diagonal entries
         for idc, gate_comb in enumerate(gate_combs_tmp):
-            if gate_comb[0] is plungers[0] and gate_comb[1] is plg:
+            if gate_comb[0] == plungers[0] and gate_comb[1] == plg:
                 cc_map['V'+plg][plg] = (1 + 1/(np.tan((ac_fits[idc][1][0] + ac_fits[idc][1][2] - np.pi)/2)*np.tan(ac_fits[idc][1][4]))) / \
                       (1 / np.tan(ac_fits[idc][1][4]) + np.tan((ac_fits[idc][1][1] + ac_fits[idc][1][3] + np.pi)/2))
         # then the first column
