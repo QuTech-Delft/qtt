bf7cd0b8c1d51feb9417375158c777f807b26efe
Pieter Eendebak <pieter.eendebak@gmail.com>
2017-09-29 14:36:08 +0200

    cleanup bluefors

--- qtt/instrument_drivers/BlueFors.py
+++ qtt/instrument_drivers/BlueFors.py
@@ -1,6 +1,5 @@
 
 #%%
-import time
 from qcodes import Instrument
 from zmqrpc.ZmqRpcClient import ZmqRpcClient
 
@@ -11,18 +10,13 @@ class BlueFors(Instrument):
     '''
     def __init__(self, name, ip_address = 'localhost', **kwargs):
         super().__init__(name, **kwargs)
-        # we need this to be a parameter (a function does not work with measure)
         
         self.client = ZmqRpcClient(zmq_req_endpoints=["tcp://%s:30000" % ip_address])
-
-
-        t=self.client.invoke('print_time')
-
 
         self.add_parameter('temperature', unit='mK', get_cmd=lambda : self.client.invoke('temperature'),
                            docstring='Temperature of cold plate')
         #_ = self.timestamp.get()
 
 #%%
-
-b=BlueFors(name='xld', ip_address='localhost')
+if __name__=='__main__':
+    b=BlueFors(name='xld', ip_address='localhost')
