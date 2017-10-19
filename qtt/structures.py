4440d495da011d151b7f46cabdf75e2181d867cf
Pieter Eendebak <pieter.eendebak@gmail.com>
2017-10-19 17:28:26 +0200

    fix has_set and has_get

--- qtt/structures.py
+++ qtt/structures.py
@@ -529,8 +529,8 @@ class CombiParameter(qcodes.instrument.parameter.Parameter):
         if unit is None:
             self.unit = 'a.u.'
 
-        self.has_get=True
-        self.has_set=True
+        #self.has_get=True
+        #self.has_set=True
         
     def get(self):
         values = []
