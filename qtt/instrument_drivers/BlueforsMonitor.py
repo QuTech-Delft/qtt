0863017745b4c83d76c51da91c1fc9119e7c57e0
Luc Blom <lcblom@tudelft.net>
2017-11-10 08:24:27 +0100

    autopep8

--- qtt/instrument_drivers/BlueforsMonitor.py
+++ qtt/instrument_drivers/BlueforsMonitor.py
@@ -7,18 +7,21 @@ from DistributedInstrument import InstrumentDataClient
 from DistributedInstrument import InstrumentDataServer
 
 
-#--------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 class FridgeDataReceiver(InstrumentDataClient):
 
     def __init__(self, name, **kwargs):
-       super().__init__(name, **kwargs)
-       self.add_measurable_quantity('temperature', 'K', -1, 'Cold plate temperature')
-       self.add_measurable_quantity('pressure', 'bar', -1, 'Maxigauge pressure')
-       self.add_measurable_quantity('datetime', '', -1, 'Read time from server')
+        super().__init__(name, **kwargs)
+        self.add_measurable_quantity('temperature', 'K', -1,
+                                     'Cold plate temperature')
+        self.add_measurable_quantity('pressure', 'bar', -1,
+                                     'Maxigauge pressure')
+        self.add_measurable_quantity('datetime', '', -1,
+                                     'Read time from server')
 
 
-#--------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 class FridgeDataSender():
 
@@ -27,17 +30,18 @@ class FridgeDataSender():
 
     def __init__(self, folder_path, **kwargs):
         self._check_folder_path_(folder_path)
-        quantities = { 'datetime' : self.get_datetime, 'temperature' : self.get_temperatures, 
-                      'pressure' : self.get_pressures }
+        quantities = {'datetime': self.get_datetime,
+                      'temperature': self.get_temperatures,
+                      'pressure': self.get_pressures}
         _data_server_ = InstrumentDataServer(quantities, **kwargs)
         _data_server_.start()
 
     def _check_folder_path_(self, path):
         self._folder_path_ = path
-        #check if folder and .../17-10-11/ exists..
+        # check if folder and .../17-10-11/ exists..
 
     def _read_file_(self, file_path):
-        with open(file_path, 'r') as fstream: 
+        with open(file_path, 'r') as fstream:
             last_line = fstream.readlines()[-1]
         return last_line.strip().split(",")
 
@@ -47,19 +51,25 @@ class FridgeDataSender():
 
     def get_temperatures(self):
         today = datetime.now().strftime('%y-%m-%d')
-        T_directory = '{0}\\{1}\\{2}'.format(self._folder_path_, today, FridgeDataSender._T_file_ext_)
+        T_directory = '{0}\\{1}\\{2}'.format(self._folder_path_, today,
+                                             FridgeDataSender._T_file_ext_)
+        print(T_directory)
         temperature_files = glob.glob(T_directory)
+        print(temperature_files)
         assert(len(temperature_files) == 5)
         T = [self._read_temperature_(file) for file in temperature_files]
-        return {'1 - PT1':T[0], '2 - PT2':T[1], '3 - Magnet':T[2], '5 - Still':T[3], '6 - MC':T[4] }
+        return {'1 - PT1': T[0], '2 - PT2': T[1], '3 - Magnet': T[2],
+                '5 - Still': T[3], '6 - MC': T[4]}
 
     def _read_pressure_(self, file_path):
         P = self._read_file_(file_path)
-        return { 'P1':float(P[5]),'P2':float(P[11]),'P3':float(P[17]),'P4':float(P[23]),'P5':float(P[29]), 'P6':float(P[35]) }
+        return {'P1': float(P[5]), 'P2': float(P[11]), 'P3': float(P[17]),
+                'P4': float(P[23]), 'P5': float(P[29]), 'P6': float(P[35])}
 
     def get_pressures(self):
         today = datetime.now().strftime('%y-%m-%d')
-        P_directory = '{0}\\{1}\\{2}'.format(self._folder_path_, today, FridgeDataSender._P_file_ext_)
+        P_directory = '{0}\\{1}\\{2}'.format(self._folder_path_, today,
+                                             FridgeDataSender._P_file_ext_)
         pressure_files = glob.glob(P_directory)
         assert(len(pressure_files) == 1)
         return self._read_pressure_(pressure_files[0])
@@ -74,7 +84,7 @@ class FridgeDataSender():
         self._data_server_.stop()
 
 
-#--------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 class BlueforsApp():
 
@@ -82,7 +92,7 @@ class BlueforsApp():
     _long_options_ = ['--help', '--dir', '--un', '--pw', '--port']
 
     def __init__(self):
-        self._directory_ = '' # todo: set default folder...
+        self._directory_ = ''  # todo: set default folder...
         self._username_ = None
         self._password_ = None
         self._port_ = 8080
@@ -94,7 +104,7 @@ class BlueforsApp():
     def print_usage(self, status=0):
         self.print_header()
         print(' BlueForsMonitor.exe [options]')
-        print('    [-h, -?, --help]   print command line usage information')
+        print('    [-h, -?, --help]   print usage information')
         print('    [-d, --dir]        sets the log-data directory')
         print('    [-n, --port]       sets the proxy port number\n')
         print('    [-u, --user]       sets the server username')
@@ -116,31 +126,44 @@ class BlueforsApp():
         self._username_ = username
 
     def set_portnumber(self, port: str):
-        try: self._port_ = int(port)
-        except ValueError: self.print_usage(-3)
+        try:
+            self._port_ = int(port)
+        except ValueError:
+            self.print_usage(-3)
 
     def set_directory(self, directory: str):
         self._directory_ = directory
 
     def check_directory(self):
-        pass #todo check if exists
+        pass  # todo check if exists
 
     def main(self, argv):
-        if len(argv) < 2 : self.print_usage(-1)
-        try: options, arguments = getopt.getopt(argv[1:], BlueforsApp._short_options_, BlueforsApp._long_options_)
-        except getopt.GetoptError: self.print_usage(-2)
+        if len(argv) < 2:
+            self.print_usage(-1)
+        try:
+            options, arguments = getopt.getopt(argv[1:],
+                                               BlueforsApp._short_options_,
+                                               BlueforsApp._long_options_)
+        except getopt.GetoptError:
+            self.print_usage(-2)
         for option, argument in options:
-            if option in ('-?', '-h', '--help'): self.print_usage()
-            elif option in ("-u", "--un"): self.set_username(argument)
-            elif option in ("-p", "--pw"): self.set_password(argument)
-            elif option in ("-d", "--dir"): self.set_directory(argument)
-            elif option in ("-n", "--port"): self.set_portnumber(argument)
+            if option in ('-?', '-h', '--help'):
+                self.print_usage()
+            elif option in ("-u", "--un"):
+                self.set_username(argument)
+            elif option in ("-p", "--pw"):
+                    self.set_password(argument)
+            elif option in ("-d", "--dir"):
+                self.set_directory(argument)
+            elif option in ("-n", "--port"):
+                self.set_portnumber(argument)
         self.print_settings()
         self.check_directory()
-        FridgeDataSender(self._directory_, port=self._port_, user=self._username_, password=self._password_)
+        FridgeDataSender(self._directory_, port=self._port_,
+                         user=self._username_, password=self._password_)
 
 
-#--------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     BlueforsApp().main(sys.argv)
@@ -150,7 +173,7 @@ if __name__ == '__main__':
 #Base examples
 
 ## Server
-directory = 'C:\\Workspace\\Qutech\\Resources\\_Other\\TestData\\Fridge'
+directory = 'D:\\workspace\\QuTech\\data\\Fridge\\17-11-10'
 FridgeDataSender(folder_path=directory)
 
 ## Client
@@ -159,5 +182,4 @@ print(client.temperature())
 print(client.pressure())
 print(client.datetime())
 
-#--------------------------------------------------------------------------------------------------
 '''
--- qtt/instrument_drivers/DistributedInstrument.py
+++ qtt/instrument_drivers/DistributedInstrument.py
@@ -4,32 +4,45 @@ from zmqrpc.ZmqRpcClient import ZmqRpcClient
 from zmqrpc.ZmqRpcServer import ZmqRpcServerThread
 
 
-#--------------------------------------------------------------------------------------------------
-# Represents a client for collecting instrument measurable quantities from a server.
+# -----------------------------------------------------------------------------
+# Represents a client for collecting instrument measurable quantities from
+# a server.
 
 class InstrumentDataClient(Instrument):
 
-    def __init__(self, name, address='localhost', port=8080, user=None, password=None, **kwargs):
+    def __init__(self, name, address='localhost', port=8080, user=None,
+                 password=None, **kwargs):
         super().__init__(name, **kwargs)
-        self._client_ = ZmqRpcClient(["tcp://{0}:{1}".format(address, port)], username=user, password=password)
+        self._client_ = ZmqRpcClient(["tcp://{0}:{1}".format(address, port)],
+                                     username=user, password=password)
 
     def __proxy_wrapper__(self, command_name, default_value, sec_time_out=3):
-        try: return self._client_.invoke(command_name, time_out_waiting_for_response_in_sec=sec_time_out)
-        except: return default_value
+        try:
+            return self._client_.invoke(command_name, None, 3)
+        except:
+            return default_value
 
-    def add_measurable_quantity(self, name='quantity', unit='arb.', default_value=None, doc_string='Unknown'):
-        command = partial(self.__proxy_wrapper__, command_name=name, default_value=default_value)
-        self.add_parameter(name, unit=unit, get_cmd=command, docstring=doc_string)
+    def add_measurable_quantity(self, name='quantity', unit='arb.',
+                                default_value=None, doc_string='Unknown'):
+        command = partial(self.__proxy_wrapper__, command_name=name,
+                          default_value=default_value)
+        self.add_parameter(name, unit=unit, get_cmd=command,
+                           docstring=doc_string)
 
 
-#--------------------------------------------------------------------------------------------------
-# Represents a server proxy for sending instrument measurable quantities to a client.
+# ------------------------------------------------------------------------------
+# Represents a server proxy for sending instrument measurable quantities to
+# a client.
 
 class InstrumentDataServer():
 
-    def __init__(self, functions, address='*', port=8080, user=None, password=None):
-        self._server_ = ZmqRpcServerThread("tcp://{0}:{1}".format(address, port), rpc_functions=functions,
-                                            username=user, password=password)
+    def __init__(self, functions, address='*', port=8080, user=None,
+                 password=None):
+        self._server_ = ZmqRpcServerThread("tcp://{0}:{1}".format(
+                                           address, port),
+                                           rpc_functions=functions,
+                                           username=user,
+                                           password=password)
 
     def start(self):
         '''Start the server proxy on the set adrress and port-number.'''
@@ -37,7 +50,8 @@ class InstrumentDataServer():
         print(' Press CTRL+C to quit!')
         try:
             self._server_.start()
-            while(True) : continue
+            while(True):
+                continue
         except KeyboardInterrupt as ex:
             print(' Done')
         finally:
@@ -45,8 +59,8 @@ class InstrumentDataServer():
             self._server_.join()
 
 
-#--------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Example for testing...
 
-if __name__=='__main__':
-    fridge=FridgeProxy(name='fridge_test', ip_address='localhost')
+if __name__ == '__main__':
+    fridge = FridgeProxy(name='fridge_test', ip_address='localhost')
