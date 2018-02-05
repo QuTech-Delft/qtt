2145121b4ad6ad7d8dda7bbb7dfcdeba7a0db542
peendebak <P.T.eendebak@tudelft.nl>
2018-02-05 13:58:07 +0100

    Tmppte (#257)
    * fix for init of averaging mode
    
    * paramviewer add setParamSingleStep

--- qtt/gui/parameterviewer.py
+++ qtt/gui/parameterviewer.py
@@ -134,6 +134,15 @@ class ParameterViewer(QtWidgets.QTreeWidget):
             print(ex)
             pass
 
+
+    def setParamSingleStep(self, instr, param, value):
+        try:
+            box=self._itemsdict[instr][param]
+            box.setSingleStep(value)
+        except Exception as ex:
+            print(ex)
+            pass
+
     def setSingleStep(self, value, instrument_name=None):
         """ Set the default step size for parameters in the viewer """
         if instrument_name is None:
