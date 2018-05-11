650e62bb20495e26681eda800c7da37da1aa6852
Pieter Eendebak <pieter.eendebak@tno.nl>
2018-05-11 10:30:27 +0200

    fix tests

--- qtt/tests/test_data.py
+++ qtt/tests/test_data.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+import unittest 
 
 import qcodes
 import qcodes.tests.data_mocks
@@ -8,15 +8,16 @@ import qtt.data
 #%%
 
 
-class Test1(TestCase):
+class TestData(unittest.TestCase):
 
     def test_transform(self):
         dataset = qcodes.tests.data_mocks.DataSet2D()
         # print(dataset)
         tr = qtt.data.image_transform(dataset, arrayname='z')
         istep = tr.istep()
+        self.assertEqual(istep, 1)
         # print(tr)
 
 if __name__ == '__main__':
-    t1 = Test1()
+    t1 = TestData()
     t1.test_transform()
