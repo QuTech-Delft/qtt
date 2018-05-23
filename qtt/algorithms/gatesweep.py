fe37b7005e7bf6c939e70b10a762de6dae067250
Pieter Eendebak <pieter.eendebak@gmail.com>
2018-05-23 19:41:43 +0200

    fix typo

--- qtt/algorithms/gatesweep.py
+++ qtt/algorithms/gatesweep.py
@@ -198,7 +198,7 @@ def analyseGateSweep(dd, fig=None, minthr=None, maxthr=None, verbose=1, drawsmoo
             plt.plot(x[leftidx], leftval, '--m', markersize=15, linewidth=1, label='leftval')
 
 
-    adata = dict({'description': 'pichoff analysis', 'pinchvalue': float(midpoint2 - 50),
+    adata = dict({'description': 'pinchoff analysis', 'pinchvalue': float(midpoint2 - 50),
                   '_pinchvalueX': midpoint1 - 50, 'goodgate': goodgate})
     adata['lowvalue'] = lowvalue
     adata['highvalue'] = highvalue
