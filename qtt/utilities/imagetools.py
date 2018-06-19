1b25263b116f42f836ae4049ca800e0ca66c744c
Fahrenfort <nora.fahrenfort@tno.nl>
2018-06-19 17:09:09 +0200

    fixed the function evaluateCross also learned how to make a commit

--- qtt/utilities/imagetools.py
+++ qtt/utilities/imagetools.py
@@ -279,7 +279,7 @@ def evaluateCross(param, im, verbose=0, fig=None, istep=1, istepmodel=1, linewid
         cost += 10000
 
     if len(param) > 7:
-        if np.abs(angleDiff(param[7], np.pi / 4)) > np.deg2rad(30):
+        if np.abs(pmatlab.angleDiff(param[7], np.pi / 4)) > np.deg2rad(30):
             #print('psi deviation!')
             cost += 10000
 
