6a15a805965d402a9ac010b4cc67cb06fcaa3947
Luc Blom <L.C.Blom@TUDelft.nl>
2018-07-15 22:03:23 +0200

    Open README with correct encoding.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import platform
 import re
 
 def readme():
-    with open('README.md') as f:
+    with open('README.md', encoding='utf-8') as f:
         return f.read()
 
 def get_version(verbose=1):
