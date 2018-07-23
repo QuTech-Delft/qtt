0ff25d0a5f0f82887933c88d2357d2d60fdb27ca
Floor van Riggelen <floor.vanriggelen@tno.nl>
2018-07-23 17:42:37 +0200

    adjustments to test_fit_gauss_ramsey

--- qtt/algorithms/functions.py
+++ qtt/algorithms/functions.py
@@ -271,15 +271,21 @@ def test_fit_gauss_ramsey(fig=None):
        0.3803, 0.3873, 0.3873, 0.3561, 0.37  , 0.3819, 0.3834, 0.3838,
        0.37  , 0.383 , 0.3573, 0.3869, 0.3838, 0.3792, 0.3757, 0.3815])
     x_data = np.array([i*1.6/40 for i in range(40)])
+    
     par_fit_test,_ = fit_gauss_ramsey(x_data*1e-6,y_data)
-    test_x = np.linspace(0,x_data.max(),200)
+    
+    assert(np.abs(np.abs(par_fit_test[0])-0.21)<0.1)
+    assert(np.abs(par_fit_test[-2]-1.88)<0.1)
+    assert(np.abs(par_fit_test[-1]-0.38)<0.1)
+    
+    test_x = np.linspace(0,x_data.max()*1e-6,200)
     test_y = gauss_ramsey(test_x,par_fit_test)
     
     if fig is not None:
         plt.figure(10);
         plt.clf()
-        plt.plot(x_data, y_data, '.b', label='input data')
-        plt.plot(test_x, test_y, '-m', label='fitted curve')
+        plt.plot(x_data, y_data, 'o', label='input data')
+        plt.plot(test_x*1e6, test_y, label='fitted curve')
         plt.legend(numpoints=1)
         
 if __name__ == '__main__':
