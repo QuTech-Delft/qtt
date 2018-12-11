b3652fc15516548a6e3bf2a89fa1cc2027adb877
Pieter Eendebak <pieter.eendebak@tno.nl>
2018-12-11 13:26:55 +0100

    autopep

--- qtt/tests/test_data.py
+++ qtt/tests/test_data.py
@@ -1,4 +1,4 @@
-import unittest 
+import unittest
 
 import qcodes
 import qcodes.tests.data_mocks
@@ -7,31 +7,32 @@ import qtt.data
 
 #%%
 
+
 class TestDataSetHelpers(unittest.TestCase):
-    
+
     def test_dataset_labels(self):
-        ds = qtt.data.makeDataSet2Dplain('horizontal', [0], 'vertical', [0], 'z', [0], xunit='mV', yunit='Hz', zunit='A')
-      
+        ds = qtt.data.makeDataSet2Dplain('horizontal', [0], 'vertical', [0], 'z', [
+                                         0], xunit='mV', yunit='Hz', zunit='A')
+
         self.assertEqual(qtt.data.dataset_labels(ds), 'z')
         self.assertEqual(qtt.data.dataset_labels(ds, 'x'), 'horizontal')
         self.assertEqual(qtt.data.dataset_labels(ds, 1), 'horizontal')
         self.assertEqual(qtt.data.dataset_labels(ds, 'y'), 'vertical')
 
         self.assertEqual(qtt.data.dataset_labels(ds, 'y', add_unit='True'), 'vertical [Hz]')
-    
+
 
 class TestData(unittest.TestCase):
 
     def test_transform(self):
         dataset = qcodes.tests.data_mocks.DataSet2D()
-        # print(dataset)
         tr = qtt.data.image_transform(dataset, arrayname='z')
         istep = tr.istep()
         self.assertEqual(istep, 1)
-        # print(tr)
+
 
 if __name__ == '__main__':
     unittest.main()
-    
+
     t1 = TestData()
     t1.test_transform()
