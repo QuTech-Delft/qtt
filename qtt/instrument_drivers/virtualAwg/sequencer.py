07419ab6000f598855719e98b049ecd8f0e20caa
CumulonimbusCalvus <9318177+CumulonimbusCalvus@users.noreply.github.com>
2019-01-28 16:47:21 +0100

    Implemented better version of pulse gates. (#494)
    * Implemented better version of pulse_gates.
    * Fix comments.
    * Removed unneeded function.

--- qtt/instrument_drivers/virtualAwg/sequencer.py
+++ qtt/instrument_drivers/virtualAwg/sequencer.py
@@ -2,7 +2,7 @@ import warnings
 import numpy as np
 from matplotlib import pyplot as plt
 
-from qupulse.pulses import SequencePT
+from qupulse.pulses import SequencePT, TablePT
 from qupulse.pulses.plotting import (PlottingNotPossibleException, plot, render)
 from qupulse.pulses.sequencing import Sequencer as Sequencing
 from qupulse.serialization import Serializer, DictBackend
@@ -59,6 +59,31 @@ class Sequencer:
                 'type': DataTypes.QU_PULSE}
 
     @staticmethod
+    def make_pulse_table(amplitudes, waiting_times, repetitions=1, name='pulse_table'):
+        """ Creates a sequence of pulses from a list of amplitudes and waiting times.
+
+        Args:
+            amplitudes (list[float]): List with voltage amplitudes of the pulses.
+            waiting_times (list[float]): List with durations containing the waiting time of each pulse.
+            repetitions (int): The number of oscillations in the sequence.
+            name (str): The name of the returned sequence.
+
+        Returns:
+            Dict: *NAME*, *TYPE*, *WAVE* keys containing values; sequence name,
+                  sequence data type and the actual qupulse sequencePT respectively.
+        """
+        if len(amplitudes) != len(waiting_times):
+            raise ValueError('Arguments have invalid lengths! (amplitudes={}, waiting_times={}'.format(
+                             len(amplitudes), len(waiting_times)))
+        time_in_ns = 0.0
+        entry_list = list()
+        for waiting_time, amplitude in zip(waiting_times, amplitudes):
+            time_in_ns += waiting_time * Sequencer.__sec_to_ns
+            entry_list.append((time_in_ns, amplitude, 'jump'))
+        sequence_data = TablePT({name: entry_list})
+        return {'name': name, 'wave': SequencePT(*(sequence_data,)*repetitions), 'type': DataTypes.QU_PULSE}
+
+    @staticmethod
     def make_marker(period, uptime=0.2, offset=0.0, repetitions=1, name='marker'):
         """ Creates a marker block waveforms of the type qupulse template.
 
@@ -105,7 +130,7 @@ class Sequencer:
         instructions = sequencer.build()
         if not sequencer.has_finished():
             raise PlottingNotPossibleException(template)
-        (_, voltages, measurements) = render(instructions, sampling_rate / Sequencer.__sec_to_ns)
+        (_, voltages, _) = render(instructions, sampling_rate / Sequencer.__sec_to_ns)
         return voltages[next(iter(voltages))]
 
     @staticmethod
@@ -292,3 +317,15 @@ def test_serializer():
         amplitude = 1.5
         sawtooth = Sequencer.make_sawtooth_wave(amplitude, period)
         Sequencer.serialize(sawtooth)
+
+
+def test_make_pulse_table():
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=UserWarning, message="qupulse")
+        amplitudes = [1, 2, 3]
+        waiting_times = [1e-4, 2e-5, 3e-3]
+        sampling_rate = 1e9
+        pulse_data = Sequencer.make_pulse_table(amplitudes, waiting_times)
+        raw_data = Sequencer.get_data(pulse_data, sampling_rate)
+        assert raw_data[0] == amplitudes[0]
+        assert raw_data[-1] == amplitudes[-1]
