cd0988aa177d9b8bcc5208d785869f5e06caf7a2
Pieter Eendebak <pieter.eendebak@tno.nl>
2019-01-29 18:12:54 +0100

    add test for exponential fitting; add option to fix offset in exponential fitting

--- qtt/algorithms/functions.py
+++ qtt/algorithms/functions.py
@@ -233,7 +233,7 @@ def exp_function(x, a, b, c):
     return y
 
 
-def cost_exp_decay(x_data, y_data, params, threshold=None):
+def cost_exp_decay(x_data, y_data, params, threshold=None, offset_parameter = None):
     """ Cost function for exponential decay.
 
     Args:
@@ -242,10 +242,14 @@ def cost_exp_decay(x_data, y_data, params, threshold=None):
         params (array): parameters of the exponential decay function, [A,B, gamma]
         threshold (float or None or 'auto'): if the difference between data and model is larger then the threshold, then the cost penalty is reduced.
             If None use normal cost function. If 'auto' use automatic detection (at 95th percentile)
+        offset_parameter (None or float): if None, then estimate the offset, otherwise fix the offset to the specified value
     Returns:
         cost (float): value which indicates the difference between the data and the fit
     """
-    model = exp_function(x_data, params[0], params[1], params[2])
+    if offset_parameter is None:
+        model = exp_function(x_data, *params)
+    else:
+        model = exp_function(x_data, offset_parameter, *params)
     cost = qtt.pgeometry.robustCost(y_data - model, thr=threshold)
     cost = np.linalg.norm(cost)
     return cost
@@ -262,9 +266,14 @@ def test_cost_exp_decay():
     assert(c == 10.0)
     c = cost_exp_decay(x_data, y_data, params, threshold='auto')
     assert(c < 10.0)
+    c = cost_exp_decay(x_data, y_data, params[1:], threshold=None, offset_parameter = 1)
+    np.testing.assert_almost_equal(c, 10.)
 
+    c = cost_exp_decay(x_data, y_data, params[1:], threshold=None, offset_parameter = 10)
+    np.testing.assert_almost_equal(c, 43.58898943540674)
 
-def fit_exp_decay(x_data, y_data, maxiter=None, maxfun=5000, verbose=1, initial_params=None, threshold=None):
+def fit_exp_decay(x_data, y_data, maxiter=None, maxfun=5000, verbose=1, initial_params=None, threshold=None,
+                  offset_parameter = None):
     """ Fit a exponential decay. 
 
     Args:
@@ -277,23 +286,41 @@ def fit_exp_decay(x_data, y_data, maxiter=None, maxfun=5000, verbose=1, initial_
         threshold (float or None): threshold for the cost function.
             If the difference between data and model is larger then the threshold, these data are not taken into account for the fit. 
             If None use automatic detection (at 95th percentile)
+        offset_parameter (None or float): if None, then estimate the offset, otherwise fix the offset to the specified value
     Returns:
-        par_fit (array): fit parameters of the exponential decay, [A,B, gamma]
+        par_fit (array): fit parameters of the exponential decay, [A, B, gamma]
+    
+    See: :func:`exp_function`
 
     """
 
-    def func(params): return cost_exp_decay(x_data, y_data, params, threshold)
+    def func(params): return cost_exp_decay(x_data, y_data, params, threshold, offset_parameter)
     if initial_params is None:
         maxsignal = np.percentile(y_data, 98)
         minsignal = np.percentile(y_data, 2)
         gamma = 1 / (x_data[int(len(x_data) / 2)])
-        A = minsignal
         B = maxsignal
-        initial_params = np.array([A, B, gamma])
+        if offset_parameter is None:
+            A = minsignal
+            initial_params = np.array([A, B, gamma])
+        else:
+            initial_params = np.array([B, gamma])
 
     par_fit = scipy.optimize.fmin(func, initial_params, maxiter=maxiter, maxfun=maxfun, disp=verbose >= 2)
+    if offset_parameter is not None:
+        par_fit = np.hstack( ([offset_parameter] ,  par_fit))
+    
     return par_fit
 
+
+def test_fit_exp_decay():
+    x_data =np.arange(0, 10., .1)
+    parameters = [0, 1, 1]
+    y_data =exp_function(x_data, *parameters)
+    fitted = fit_exp_decay(x_data, y_data)
+    np.testing.assert_array_almost_equal(fitted, parameters, decimal=3)
+    fitted = fit_exp_decay(x_data, y_data, offset_parameter=0.1)
+    np.testing.assert_array_almost_equal(fitted, [.1, .95, 1.4], decimal=1)
 
 def gauss_ramsey(x_data, params):
     """ Model for the measurement result of a pulse Ramsey sequence while varying the free evolution time, the phase of the second pulse 
