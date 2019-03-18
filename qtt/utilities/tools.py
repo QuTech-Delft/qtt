b50e9f8a033b35791232b0dfbdb64810c431c6f6
Pieter Eendebak <P.T.eendebak@tudelft.nl>
2019-03-18 20:55:55 +0100

    Update tools.py

--- qtt/utilities/tools.py
+++ qtt/utilities/tools.py
@@ -861,14 +861,14 @@ def _ppt_determine_image_position(ppt, figsize, fname, verbose=1):
 
 
 
-def create_figure_ppt_callback(fig, title='', notes=None, position=(0.9, 0.925, 0.075, 0.05) ):
+def create_figure_ppt_callback(fig, title=None, notes=None, position=(0.9, 0.925, 0.075, 0.05) ):
     """ Create a callback on a matplotlib figure to copy data to PowerPoint slide
 
     The figure is copied to PowerPoint using @ref addPPTslide.
 
     Args:
         fig (int): handle to matplotlib window
-        title (str): title for the slide
+        title (None or str): title for the slide
         notes (None or str or DataSet): notes to add to the slide
         position (list): position specified as fraction left, right, width, height
     """
