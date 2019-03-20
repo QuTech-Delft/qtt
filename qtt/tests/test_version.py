QuTech-Delft/qtt

commit cc994777fdffc45d5109199d454f8d33169ceb54

Do not use deprecated numpy.matrix (#507)
* Do not use deprecated numpy matrix.
* Fix warnings in tests; update docstrings.

qtt/pgeometry.py
@@ -561,13 +561,13 @@ def opencvpose2attpos(rvecs, tvecs):
     rvec = np.array(rvecs).flatten()
     R, tmp = cv2.Rodrigues(rvec)
     att = RBE2euler(R)
-    pos = -R.transpose().dot(np.matrix(tvec.reshape((3, 1))))
+    pos = -R.transpose().dot(np.array(tvec.reshape((3, 1))))
     return att, pos
 
 
 def opencv2TX(rvecs, tvecs):
     """ Convert OpenCV pose to homogenous transform """
-    T = np.matrix(np.eye(4))
+    T = np.array(np.eye(4))
     R = cv2.Rodrigues(rvecs)[0]
     T[0:3, 0:3] = R
     T[0:3, 3:4] = tvecs
@@ -576,7 +576,7 @@ def opencv2TX(rvecs, tvecs):
 
 def opencv2T(rvec, tvec):
     """ Convert OpenCV pose to homogenous transform """
-    T = np.matrix(np.eye(4))
+    T = np.array(np.eye(4))
     T[0:3, 0:3] = cv2.Rodrigues(rvec)[0]
     T[0:3, 3] = tvec
     return T
@@ -602,9 +602,9 @@ def euler2RBE(theta):
       -------
       >>> np.set_printoptions(precision=4, suppress=True)
       >>> euler2RBE( [0,0,np.pi/2] )
-      matrix([[ 0., -1.,  0.],
-              [ 1.,  0.,  0.],
-              [-0.,  0.,  1.]])
+      array([[ 0., -1.,  0.],
+             [ 1.,  0.,  0.],
+             [-0.,  0.,  1.]])
 
     """
     cr = math.cos(theta[0])
@@ -614,7 +614,7 @@ def euler2RBE(theta):
     cy = math.cos(theta[2])
     sy = math.sin(theta[2])
 
-    out = np.matrix([cp * cy, sr * sp * cy - cr * sy, cr * sp * cy + sr * sy,
+    out = np.array([cp * cy, sr * sp * cy - cr * sy, cr * sp * cy + sr * sy,
                      cp * sy, sr * sp * sy + cr * cy, cr * sp * sy - sr * cy, -sp, sr * cp, cr * cp])
     return out.reshape((3, 3))
 
@@ -632,7 +632,7 @@ def RBE2euler(Rbe):
 
 def pg_rotation2H(R):
     """ Convert rotation matrix to homogenous transform matrix """
-    X = np.matrix(np.eye((R.shape[0] + 1)))
+    X = np.array(np.eye((R.shape[0] + 1)))
     X[0:-1, 0:-1] = R
     return X
 
@@ -839,7 +839,7 @@ def pg_transl2H(tr):
     """ Convert translation to homogeneous transform matrix
 
     >>> pg_transl2H( [1,2])
-    matrix([[ 1.,  0.,  1.],
+    array([[ 1.,  0.,  1.],
             [ 0.,  1.,  2.],
             [ 0.,  0.,  1.]])
 
@@ -847,7 +847,7 @@ def pg_transl2H(tr):
     sh = np.array(tr)
     H = np.eye(sh.size + 1)
     H[0:-1, -1] = sh.flatten()
-    H = np.matrix(H)
+    H = np.array(H)
     return H
 
 
@@ -2132,12 +2132,12 @@ def decomposeProjectiveTransformation(H, verbose=0):
 
     >>> Ha, Hs, Hp, rest = decomposeProjectiveTransformation( np.eye(3) )
     """
-    H = np.matrix(H)
+    H = np.array(H)
     k = H.shape[0]
     km = k - 1
 
     eta = H[k - 1, k - 1]
-    Hp = np.matrix(np.vstack((np.eye(km, k), H[k - 1, :])))
+    Hp = np.array(np.vstack((np.eye(km, k), H[k - 1, :])))
     A = H[0:km, 0:km]
     t = H[0:km, -1]
     v = H[k - 1, 0:km].T
@@ -2146,11 +2146,11 @@ def decomposeProjectiveTransformation(H, verbose=0):
     if np.abs(np.linalg.det(A)) < 4 * eps:
         print('decomposeProjectiveTransformation: part A of matrix is (near) singular')
 
-    sRK = A - np.matrix(t).dot(np.matrix(v.T))
+    sRK = A - np.array(t).dot(np.array(v.T))
     # upper left block of H*inv(Hp)
     R, K = np.linalg.qr(sRK)
-    K = np.matrix(K)
-    R = np.matrix(R)
+    K = np.array(K)
+    R = np.array(R)
 
     s = (np.abs(np.linalg.det(K)))**(1. / km)
     K = K / s
@@ -2164,8 +2164,8 @@ def decomposeProjectiveTransformation(H, verbose=0):
         K = np.diag(sc) * K
         R = R * np.diag(sc)
     br = np.hstack((np.zeros((1, km)), np.ones((1, 1))))
-    Hs = np.matrix(np.vstack((np.hstack((s * R, t.reshape((-1, 1)))), br)))
-    Ha = np.matrix(np.vstack((np.hstack((K, np.zeros((km, 1)))), br)))
+    Hs = np.array(np.vstack((np.hstack((s * R, t.reshape((-1, 1)))), br)))
+    Ha = np.array(np.vstack((np.hstack((K, np.zeros((km, 1)))), br)))
 
     phi = np.arctan2(R[1, 0], R[0, 0])
 

qtt/measurements/scans.py
@@ -250,15 +250,15 @@ def get_instrument(instr, station=None):
 
 
 def test_get_instrument_parameter():
-    i = qtt.instrument_drivers.virtual_instruments.VirtualIVVI(qtt.measurements.scans.instrumentName('test'), None)
-    ix, p = get_instrument_parameter((i.name, 'dac2'))
-    assert (id(ix) == id(i))
-    assert (id(p) == id(i.dac2))
-    ix, p = get_instrument_parameter((i, 'dac2'))
-    assert (id(p) == id(i.dac2))
-    ix, p = get_instrument_parameter(i.name + '.dac2')
-    assert (id(p) == id(i.dac2))
-
+    instrument = qtt.instrument_drivers.virtual_instruments.VirtualIVVI(qtt.measurements.scans.instrumentName('test'), None)
+    ix, p = get_instrument_parameter((instrument.name, 'dac2'))
+    assert (id(ix) == id(instrument))
+    assert (id(p) == id(instrument.dac2))
+    ix, p = get_instrument_parameter((instrument, 'dac2'))
+    assert (id(p) == id(instrument.dac2))
+    ix, p = get_instrument_parameter(instrument.name + '.dac2')
+    assert (id(p) == id(instrument.dac2))
+    instrument.close()
 
 def get_minstrument_channels(minstrument):
     if isinstance(minstrument, tuple):
@@ -631,7 +631,7 @@ def test_sample_data():
     assert (v == 100)
 
 def _convert_vectorname_to_parametername(vector_name, extra_string = None):
-    parameter_name = re.sub('\(.*?\)', '', vector_name)
+    parameter_name = re.sub(r'\(.*?\)', '', vector_name)
     if extra_string is not None:
         parameter_name += '_'  + extra_string
     return parameter_name

qtt/measurements/storage.py
@@ -154,3 +154,6 @@ def test_load_save_state():
     tmpfile = tempfile.mktemp()
     tag = save_state(station, virtual_gates = virts, statefile=tmpfile)
     r = load_state(station=station, tag=tag, verbose=1, statefile=tmpfile)
+    
+    virts.close()
+    

commit 5c678f56e90ab055ecfd5d1382dae9388bcfcfa8

increase version tag; enable mypy (#523)
* Increase version tag.
* Fix mypy errors.
* Add mypy to travis.
* Improve test and fix bug introduced in #507.
* Fix conditional function addPPTslide.
* Added .mypy_cache to gitignore.

.gitignore
@@ -42,7 +42,7 @@ htmlcov/
 .tox/
 .coverage
 .coverage.*
-.cache
+.*_cache
 nosetests.xml
 coverage.xml
 *,cover

.travis.yml
@@ -16,6 +16,7 @@ install:
     - pip install qcodes
     - pip install -r requirements.txt
     - pip install opencv-python coveralls
+    - pip install mypy
     - if [[ "$FULL_INSTALL" == "1" ]]; then
         echo "Full Installation";
         pip install -r develop_requirements.txt;
@@ -26,6 +27,7 @@ script:
     - ls
     - pytest --version
     - coverage run --source="./qtt" --omit="*/test_*"  -m pytest
+    - mypy qtt --ignore-missing-imports
     - if [[ "$FULL_INSTALL" == "1" ]]; then
         jupyter --version;
         source docs/notebooks/run_notebooks.sh;

qtt/__init__.py
@@ -149,8 +149,6 @@ abort_measurements = _abort_measurement
 # patch the qcodes abort function
 qcodes.loops.abort_measurements = _abort_measurement
 
-qtt._dummy_mc = []
-
 # %% Override default location formatter
 
 from qcodes.data.location import FormatLocation
@@ -173,7 +171,7 @@ def _copy_to_str(x, memo):
 # black magic to make qcodes objects work with deepcopy
 from qcodes import Parameter, Instrument, StandardParameter, ManualParameter, Station
 for c in [Parameter, Instrument, StandardParameter, ManualParameter, Station]:
-    copy._deepcopy_dispatch[c] = _copy_to_str
+    copy._deepcopy_dispatch[c] = _copy_to_str # type: ignore
 
 # make a qcodes instrument pickable
 qcodes.Instrument.__getstate__ = lambda self: str(self)

qtt/algorithms/tunneling.py
@@ -127,12 +127,12 @@ def fit_pol_all(x_data, y_data, kT, model='one_ele', maxiter=None, maxfun=5000, 
 
     fitdata = {}
     if method is 'fmin':
-        def func(par): return polweight_all_2slopes(x_data, y_data, par, kT, model=model)
-        par_fit = scipy.optimize.fmin(func, par_guess, maxiter=maxiter, maxfun=maxfun, disp=verbose >= 2)
+        def func_fmin(par): return polweight_all_2slopes(x_data, y_data, par, kT, model=model)
+        par_fit = scipy.optimize.fmin(func_fmin, par_guess, maxiter=maxiter, maxfun=maxfun, disp=verbose >= 2)
     elif method is 'curve_fit':
-        def func(x_data, tc, x0, y0, ml, mr, h): return polmod_all_2slopes(
+        def func_curve_fit(x_data, tc, x0, y0, ml, mr, h): return polmod_all_2slopes(
             x_data, (tc, x0, y0, ml, mr, h), kT, model=model)
-        par_fit, par_cov = scipy.optimize.curve_fit(func, x_data, y_data, par_guess)
+        par_fit, par_cov = scipy.optimize.curve_fit(func_curve_fit, x_data, y_data, par_guess)
         fitdata['par_cov'] = par_cov
     else:
         raise Exception('Unrecognized fitting method')

qtt/deprecated/linetools.py
@@ -439,9 +439,3 @@ def fitLine(alldata, param0=None, fig=None):
     plt.xlabel(cgate)
     plt.ylabel(igate)
 
-
-if __name__ == '__main__' and 0:
-    param0 = [0, 0, .5 * np.pi]  # x,y,theta,
-    figl = 100
-
-    fitdata = fitLine(im, param0=None, fig=None)

qtt/gui/dataviewer.py
@@ -475,10 +475,9 @@ if __name__ == '__main__':
 
     app = pg.mkQApp()
 
-    dataviewer = DataViewer(datadir=datadir, extensions=['dat', 'hdf5'])
+    dataviewer = DataViewer(data_directory=datadir, extensions=['dat', 'hdf5'])
     dataviewer.verbose = 5
     dataviewer.setGeometry(1280, 60, 700, 900)
     dataviewer.logtree.setColumnWidth(0, 240)
     dataviewer.show()
 
-    # app.exec()

qtt/gui/live_plotting.py
@@ -249,7 +249,6 @@ def start_measurement_control(doexec=False):
     proc = mp.QtProcess()
     lp = proc._import('qtt.gui.live_plotting')
     mc = lp.MeasurementControl()
-    qtt._dummy_mc = mc
     app = pyqtgraph.mkQApp()
     if doexec:
         app.exec()

qtt/instrument_drivers/FPGA_ave.py
@@ -355,8 +355,4 @@ class FPGA_ave(VisaInstrument):
         return totalpoints, DataRead_ch1, DataRead_ch2
 
 
-# %% Testing driver functionality
-if __name__ == '__main__':
-    server_name = None
-    fpga = FPGA_ave_x('FPGA', 'ASRL4::INSTR', server_name=server_name)
-    self = fpga
+

qtt/legacy.py
@@ -302,7 +302,7 @@ from qtt.utilities.imagetools import cleanSensingImage
 fitBackground = qtt.utilities.tools.deprecated(fitBackgroundTmp)
 
 
-@qtt.tools.deprecated
+@qtt.utilities.tools.deprecated
 def showIm(ims, fig=1, title='', showz=False):
     """ Show image with nearest neighbor interpolation and axis scaling """
     plt.figure(fig)

qtt/measurements/videomode.py
@@ -300,7 +300,7 @@ class VideoMode:
                         d = self.scanparams['sweepparams'][parameter]
                         for g, f in d.items():
                             if verbose >= 2:
-                                print('  %: increment %s with %s' % (parameter, g, f * delta))
+                                print('  %s: increment %s with %s' % (parameter, g, f * delta))
                             param = getattr(station.gates, g)
                             param.increment(f * delta)
                     else:
@@ -647,16 +647,10 @@ if __name__ == '__main__':
 
     from qtt.instrument_drivers.simulation_instruments import SimulationDigitizer
     from qtt.instrument_drivers.simulation_instruments import SimulationAWG
-    import pdb
-    from imp import reload
-    import matplotlib.pyplot as plt
+    #from qtt.gui.live_plotting import *
 
-    reload(qtt.gui.live_plotting)
-    from qtt.gui.live_plotting import *
+    pv = qtt.createParameterWidget([gates])  # type: ignore
 
-    pv = qtt.createParameterWidget([gates])
-
-    reload(qtt.measurements.scans)
     verbose = 1
     multiprocess = False
 
@@ -682,15 +676,11 @@ if __name__ == '__main__':
                    resolution=resolution, sample_rate='default', diff_dir=None,
                    verbose=1, nplots=None, dorun=True)
 
-    if 0:
-        sweepparams = {'gates_horz': {'P1': 1}, 'gates_vert': {'P2': 1}}
-
-        vm = VideoMode(station, sweepparams, sweepranges=[120] * 2,
-                       minstrument=minstrument, resolution=[12] * 2, Naverage=2)
-        vm.stop()
-
-    self = vm
     vm.setGeometry(1310, 100, 800, 800)
+
+    vm.stopreadout()
+    vm.updatebg()
+    vm.stop()
 
     # %% Test MultiTracePlot
     app = pyqtgraph.mkQApp()
@@ -711,8 +701,6 @@ if __name__ == '__main__':
 
     # %%
     import qtt.measurements.ttrace
-
-    reload(qtt.measurements.ttrace)
     from qtt.measurements.ttrace import MultiTracePlot
 
     mt = MultiTracePlot(nplots=nplots, ncurves=ncurves)
@@ -728,3 +716,4 @@ if __name__ == '__main__':
     mt.updatefunction()
 
     mt.get_dataset()
+    mt.stopreadout()

qtt/pgeometry.py
@@ -1576,7 +1576,7 @@ try:
         draw.text(pos, txt, fill=color, font=font)
         return np.array(im1)
 except:
-    def writeTxt(*args, **kwargs):
+    def writeTxt(im, txt, pos=(10, 10), fontsize=25, color=(0, 0, 0), fonttype=None):
         """ Dummy function """
         warnings.warn('writeTxt: could not find PIL')
         return None
@@ -2166,8 +2166,8 @@ def decomposeProjectiveTransformation(H, verbose=0):
     else:
         # primitive...
         sc = np.sign(np.diag(K))
-        K = np.diag(sc) * K
-        R = R * np.diag(sc)
+        K = np.diag(sc).dot(K)
+        R = R.dot( np.diag(sc))
     br = np.hstack((np.zeros((1, km)), np.ones((1, 1))))
     Hs = np.array(np.vstack((np.hstack((s * R, t.reshape((-1, 1)))), br)))
     Ha = np.array(np.vstack((np.hstack((K, np.zeros((km, 1)))), br)))

qtt/simulation/virtual_dot_array.py
@@ -48,17 +48,6 @@ qtt.check_version('0.24', qtt)
 
 # %% Data for the model
 
-class virtualdot_t:
-
-    def __init__(self, ndots):
-        self.ndots = ndots
-        raise Exception('do not use this class any more')
-
-    def ndots(self):
-        """ Return number of dots in the sample """
-        return self.ndots
-
-
 def gate_settle(gate):
     """ Return gate settle times """
 

qtt/utilities/markup.py
@@ -8,7 +8,7 @@ __doc__ = """
 This is markup.py - a Python module that attempts to
 make it easier to generate HTML/XML from a Python program
 in an intuitive, lightweight, customizable and pythonic way.
-It works with both python 2 and 3.
+It works with python 3 (to make mypy happy)!
 
 The code is in the public domain.
 
@@ -21,15 +21,6 @@ ideas or questions to nogradi at gmail dot com.
 
 Installation: drop markup.py somewhere into your Python path.
 """ % (__version__, __date__)
-
-try:
-    basestring
-    import string
-except:
-    # python 3
-    basestring = str
-    string = str
-    long = int
 
 # tags which are reserved python keywords will be referred
 # to by a leading underscore otherwise we end up with a syntax error
@@ -350,7 +341,7 @@ class page:
         """This convenience function is only useful for html.
         It adds css stylesheet(s) to the document via the <link> element."""
 
-        if isinstance(filelist, basestring):
+        if isinstance(filelist, str):
             self.link(
                 href=filelist, rel='stylesheet', type='text/css', media='all')
         else:
@@ -446,11 +437,11 @@ def _argsdicts(args, mydict):
 
 
 def _totuple(x):
-    """Utility stuff to convert string, int, long, float, None or anything to a usable tuple."""
+    """Utility stuff to convert string, int, float, None or anything to a usable tuple."""
 
-    if isinstance(x, basestring):
+    if isinstance(x, str):
         out = x,
-    elif isinstance(x, (int, long, float)):
+    elif isinstance(x, (int, float)):
         out = str(x),
     elif x is None:
         out = None,
@@ -463,7 +454,7 @@ def _totuple(x):
 def escape(text, newline=False):
     """Escape special html characters."""
 
-    if isinstance(text, basestring):
+    if isinstance(text, str):
         if '&' in text:
             text = text.replace('&', '&amp;')
         if '>' in text:
@@ -487,7 +478,7 @@ _escape = escape
 def unescape(text):
     """Inverse of escape."""
 
-    if isinstance(text, basestring):
+    if isinstance(text, str):
         if '&amp;' in text:
             text = text.replace('&amp;', '&')
         if '&gt;' in text:
@@ -502,14 +493,13 @@ def unescape(text):
 
 class dummy:
 
-    """A dummy class for attaching attributes."""
-    pass
-
+    def __init__(self):
+        """ A dummy class for attaching attributes."""
+        self.frameset = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">"""
+        self.strict = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">"""
+        self.loose = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">"""
 
 doctype = dummy()
-doctype.frameset = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">"""
-doctype.strict = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">"""
-doctype.loose = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">"""
 
 
 class russell:

qtt/utilities/tools.py
@@ -1137,9 +1137,10 @@ try:
         return ppt, slide
 
 except ImportError:
-    def addPPTslide(title=None, fig=None, subtitle=None, maintext=None,
-                    notes=None, show=False, verbose=1, ppLayout=1):
-        """ Dummy implementation """
+    def addPPTslide(title=None, fig=None, txt=None, notes=None, figsize=None,
+                    subtitle=None, maintext=None, show=False, verbose=1,
+                    activate_slide=True, ppLayout=None, extranotes=None, background_color=None):
+        ''' Dummy implementation '''
         warnings.warn('addPPTslide is not available on your system')
 
     def addPPT_dataset(dataset, title=None, notes=None,

qtt/version.py
@@ -1,1 +1,1 @@
-__version__ = '1.1.1'
+__version__ = '1.1.2'

qtt/tests/test_pgeometry.py
@@ -20,10 +20,16 @@ class TestGeometryOperations(unittest.TestCase):
         H = pgeometry.pg_rotation2H(R)
         np.testing.assert_almost_equal(R, H[:3, :3])
 
+    def test_decomposeProjectiveTransformation(self):
+        R = pgeometry.pg_rotation2H(pgeometry.pg_rotx(np.pi / 2))
+        Ha, Hs, Hp, _ = pgeometry.decomposeProjectiveTransformation(R)
+        self.assertIsInstance(Ha, np.ndarray)
+        np.testing.assert_array_almost_equal(Hs @ Ha @ Hp, R)
+
+        R = pgeometry.pg_rotation2H(pgeometry.pg_rotx(.012))
+        Ha, Hs, Hp, _ = pgeometry.decomposeProjectiveTransformation(R)
+        np.testing.assert_array_almost_equal(Hs @ Ha @ Hp, R)
 
 if __name__ == "__main__":
-    """ Dummy main for testing
-    """
-
     import unittest
     unittest.main()

qtt/tests/test_version.py
@@ -1,0 +1,19 @@
+import unittest
+
+import qtt
+
+
+# %%
+
+
+class TestVersion(unittest.TestCase):
+
+    def test_version(self):
+        version = qtt.__version__
+
+        self.assertIsInstance(version, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
+