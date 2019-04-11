545871f50d089acb754b0f2c2b99c24ee825c9d2
Pieter Eendebak <pieter.eendebak@gmail.com>
2019-04-11 19:22:23 +0200

    fix codacy warnings

--- src/qtt/instrument_drivers/TimeStamp.py
+++ src/qtt/instrument_drivers/TimeStamp.py
@@ -12,11 +12,9 @@ class TimeStampInstrument(Instrument):
 
         self.add_parameter('timestamp', unit='s', get_cmd=self._get_timestamp,
                            docstring='Timestamp based on number of seconds since timestamp offset.')
-        self.add_parameter('timestamp_offset', unit='s', get_cmd=None, set_cmd = None, initial_value = 0,
+        self.add_parameter('timestamp_offset', unit='s', get_cmd=None, set_cmd=None, initial_value=0,
                            docstring='Timestamp offset. Default value is the epoch.')
         _ = self.timestamp.get()
 
     def _get_timestamp(self):
         return time.time() - self.timestamp_offset()
-    
-    
--- src/tests/test_timestamp_instrument.py
+++ src/tests/test_timestamp_instrument.py
@@ -1,12 +1,8 @@
-import unittest
 from unittest import TestCase
 from unittest.mock import patch
 
 import qtt.measurements.scans
 import qtt.instrument_drivers.TimeStamp
-
-# %%
-
 
 class TestTimeStampInstrument(TestCase):
 
@@ -20,7 +16,6 @@ class TestTimeStampInstrument(TestCase):
     @patch('time.time', return_value=0.124)
     def test_TimeStampInstrument(self, time_method):
         timestamp = self.timestamp_instrument.timestamp()
-        assert(isinstance(timestamp, float))
         self.assertEqual(timestamp, 0.124)
         time_method.assert_called_with()
 
@@ -31,11 +26,8 @@ class TestTimeStampInstrument(TestCase):
 
     def test_TimeStampInstrument_offset(self):
         timestamp = self.timestamp_instrument.timestamp()
+        self.assertIsInstance(timestamp, float)
         self.timestamp_instrument.timestamp_offset(timestamp)
         timestamp = self.timestamp_instrument.timestamp()
         self.assertTrue(timestamp >= 0)
         self.assertTrue(timestamp <= 1)
-
-
-if __name__ == '__main__':
-    unittest.main()
