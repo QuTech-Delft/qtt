94b587c793c5401b5288da6b80d6207900cffad1
Ammar <ammar.abdulamir@gmail.com>
2019-04-17 11:29:32 +0200

    Implemented unit tests.

--- src/qtt/measurements/new/signal_processor.py
+++ src/qtt/measurements/new/signal_processor.py
@@ -9,14 +9,25 @@ class SignalProcessor:
     """ A class for post-processing measurement data. """
 
     def __init__(self) -> None:
-        self.signal_processors: List[SignalProcessorInterface] = []
+        self._signal_processors: List[SignalProcessorInterface] = []
 
-    def run_processes(self, signal_data: DataSet) -> None:
+    def add_signal_processor(self, signal_processor: SignalProcessorInterface) -> None:
+        """ Add a signal processor.
+
+            Args:
+                signal_processor: A signal processor
+        """
+
+        self._signal_processors.append(signal_processor)
+
+    def run_processes(self, signal_data: DataSet) -> DataSet:
         """ Post-process measurement data using the known signal processors.
 
             Args:
                 signal_data: The measurement data
         """
 
-        for signal_processor in self.signal_processors:
-            signal_processor.run_process(signal_data)
+        for signal_processor in self._signal_processors:
+            signal_data = signal_processor.run_process(signal_data)
+
+        return signal_data
