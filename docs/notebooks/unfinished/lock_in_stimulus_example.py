445bbcb0f51df62a6bad536e3fba75087e2e7450
qSaevar <saevar.steinn@gmail.com>
2019-04-26 16:40:50 +0200

    forcing an ignored unfinished example

---
+++ docs/notebooks/unfinished/lock_in_stimulus_example.py
@@ -0,0 +1,19 @@
+from qtt.measurements.acquisition import LockInStimulus
+
+lock_in_stimulus = LockInStimulus('dev2338')
+
+
+channel = 2
+oscillator_frequency = 145e6
+amplitude = 0.1
+demodulator = 1
+demodulator_enabled = True
+signal_output_enabled = True
+output_enabled = True
+output = 1
+
+lock_in_stimulus.set_oscillator_frequency(channel, oscillator_frequency)
+lock_in_stimulus.set_signal_output_amplitude(channel, demodulator, amplitude)
+lock_in_stimulus.set_demodulation_enabled(channel, demodulator_enabled)
+lock_in_stimulus.set_signal_output_enabled(channel, demodulator, signal_output_enabled)
+lock_in_stimulus.set_output_enabled(output, output_enabled)
