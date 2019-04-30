3f921c3de8f554556c1473cf6c71780e661ae080
qSaevar <39056113+qSaevar@users.noreply.github.com>
2019-04-30 13:24:52 +0200

    [DEM-927] Implement the LockInStimulus interface. (#560)
    * [DEM-927] Implement the LockInStimulus interface.
    
    * Adding qilib to requirements
    
    * removing the StimulusInterface
    
    * Renaming for consistency

---
+++ src/qtt/measurements/acquisition/uhfli_stimulus.py
@@ -0,0 +1,84 @@
+from qilib.configuration_helper import InstrumentAdapterFactory
+from qilib.utils import PythonJsonStructure
+
+
+class UHFLIStimulus:
+    """ A wrapper that provides methods to control Lock-in of Zurich instrument's UHF-LI devices."""
+
+    def __init__(self, address: str) -> None:
+        """ Instantiate a LockInStimulus.
+
+        Args:
+            address: A unique ID of a UHFLI device.
+
+        """
+        self._adapter = InstrumentAdapterFactory.get_instrument_adapter('ZIUHFLIInstrumentAdapter', address)
+        self._uhfli = self._adapter.instrument
+
+    def initialize(self, configuration: PythonJsonStructure) -> None:
+        """ Apply configuration to the UHFLI.
+
+        Args:
+            configuration: The configuration for the UHFLI.
+
+        """
+        self._adapter.apply(configuration)
+
+    def set_demodulation_enabled(self, channel: int, is_enabled: bool) -> None:
+        """ Enable data transfer in the demodulators.
+
+        Args:
+             channel: The channel number to enable (1 - 8).
+             is_enabled: True to enable and False to disable.
+
+        """
+        enabled = 'ON' if is_enabled else 'OFF'
+        qcodes_parameter_name = 'demod{}_streaming'.format(channel)
+        self._uhfli.parameters[qcodes_parameter_name](enabled)
+
+    def set_output_enabled(self, output: int, is_enabled: bool) -> None:
+        """ Control the outputs on the device.
+
+        Args:
+            output: One of the two outputs on the device.
+            is_enabled: True to enable and False to disable.
+
+        """
+        enabled = 'ON' if is_enabled else 'OFF'
+        qcodes_parameter_name = 'signal_output{}_on'.format(output)
+        self._uhfli.parameters[qcodes_parameter_name](enabled)
+
+    def set_oscillator_frequency(self, channel: int, frequency: float) -> None:
+        """ Set the oscillators frequencies.
+
+        Args:
+            channel: Channel the the oscillator belongs to (1 - 8) with MF enabled.
+            frequency: Allowed frequencies are 0 - 600 MHz.
+
+        """
+        qcodes_parameter_name = 'oscillator{}_freq'.format(channel)
+        self._uhfli.parameters[qcodes_parameter_name](frequency)
+
+    def set_signal_output_enabled(self, channel: int, demodulator: int, is_enabled: bool) -> None:
+        """ Enable one of the 16 output amplitudes.
+
+        Args:
+            channel: The channel to enable (1 - 8).
+            demodulator: Which demodulator the channel belongs to.
+            is_enabled: True to enable and False to disable.
+
+        """
+        qcodes_parameter_name = 'signal_output{}_enable{}'.format(demodulator, channel)
+        self._uhfli.parameters[qcodes_parameter_name](is_enabled)
+
+    def set_signal_output_amplitude(self, channel: int, demodulator: int, amplitude: float) -> None:
+        """ Set the amplitude of the output signal.
+
+        Args:
+            channel: The channel to set the amplitude on (1 - 8).
+            demodulator: Which demodulator the channel belongs to.
+            amplitude: Amplitude in volts, allowed values are 0.0 - 1.5 V.
+
+        """
+        qcodes_parameter_name = 'signal_output{}_amplitude{}'.format(demodulator, channel)
+        self._uhfli.parameters[qcodes_parameter_name](amplitude)
---
+++ src/tests/measurements/acquisition/test_uhfli_stimulus.py
@@ -0,0 +1,69 @@
+import unittest
+
+from mock import patch, MagicMock, call
+from qilib.utils import PythonJsonStructure
+
+from qtt.measurements.acquisition import UHFLIStimulus
+
+
+class TestLockInStimulus(unittest.TestCase):
+    def setUp(self):
+        self.adapter_mock = MagicMock()
+        self.uhfli_mock = MagicMock()
+        self.adapter_mock.instrument = self.uhfli_mock
+        with patch('qtt.measurements.acquisition.uhfli_stimulus.InstrumentAdapterFactory') as factory_mock:
+            factory_mock.get_instrument_adapter.return_value = self.adapter_mock
+            self.uhfli_stimulus = UHFLIStimulus('mock42')
+
+    def test_initialize(self):
+        config = PythonJsonStructure(bla='blu')
+        self.uhfli_stimulus.initialize(config)
+        self.adapter_mock.apply.assert_called_once_with(config)
+
+    def test_set_demodulation_enabled(self):
+        yamock = MagicMock()
+        self.uhfli_mock.parameters.__getitem__ = yamock
+
+        self.uhfli_stimulus.set_demodulation_enabled(2, True)
+        expected_calls = [call('demod2_streaming'), call()('ON')]
+        yamock.assert_has_calls(expected_calls)
+
+        self.uhfli_stimulus.set_demodulation_enabled(1, False)
+        expected_calls = [call('demod1_streaming'), call()('OFF')]
+        yamock.assert_has_calls(expected_calls)
+
+    def test_set_output_enabled(self):
+        yamock = MagicMock()
+        self.uhfli_mock.parameters.__getitem__ = yamock
+
+        self.uhfli_stimulus.set_output_enabled(2, True)
+        expected_calls = [call('signal_output2_on'), call()('ON')]
+        yamock.assert_has_calls(expected_calls)
+
+        self.uhfli_stimulus.set_output_enabled(1, False)
+        expected_calls = [call('signal_output1_on'), call()('OFF')]
+        yamock.assert_has_calls(expected_calls)
+
+    def test_set_oscillator_frequency(self):
+        yamock = MagicMock()
+        self.uhfli_mock.parameters.__getitem__ = yamock
+
+        self.uhfli_stimulus.set_oscillator_frequency(5, 42.0)
+        expected_calls = [call('oscillator5_freq'), call()(42.0)]
+        yamock.assert_has_calls(expected_calls)
+
+    def test_set_signal_output_enabled(self):
+        yamock = MagicMock()
+        self.uhfli_mock.parameters.__getitem__ = yamock
+
+        self.uhfli_stimulus.set_signal_output_enabled(3, 2, True)
+        expected_calls = [call('signal_output2_enable3'), call()(True)]
+        yamock.assert_has_calls(expected_calls)
+
+    def test_set_signal_output_amplitude(self):
+        yamock = MagicMock()
+        self.uhfli_mock.parameters.__getitem__ = yamock
+
+        self.uhfli_stimulus.set_signal_output_amplitude(7, 1, 0.42)
+        expected_calls = [call('signal_output1_amplitude7'), call()(0.42)]
+        yamock.assert_has_calls(expected_calls)
