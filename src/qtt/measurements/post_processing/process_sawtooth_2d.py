5e0af1615cd47801e84f9abd72e90132148f1541
CumulonimbusCalvus <9318177+CumulonimbusCalvus@users.noreply.github.com>
2019-05-03 10:34:31 +0200

    Fixed codecy.

--- src/qtt/measurements/post_processing/process_sawtooth_2d.py
+++ src/qtt/measurements/post_processing/process_sawtooth_2d.py
@@ -6,22 +6,22 @@ from qtt.measurements.post_processing import SignalProcessorInterface
 
 class ProcessSawtooth2D(SignalProcessorInterface):
 
-    def run_process(self, data_set: DataSet)-> DataSet:
+    def run_process(self, signal_data: DataSet)-> DataSet:
         """ Extracts a 2D image from a readout dot responce measured with an acquisition device.
 
         Args:
-            data_set: The readout dot reponse data coming from the acquisition device. The data
-                      user data of the data set should contain the width and resolution settings.
+            signal_data: The readout dot reponse data coming from the acquisition device. The data
+                         user data of the data set should contain the width and resolution settings.
 
         Returns:
             A data set which contains a 2D image with the charge stability diagram.
         """
-        width_x, width_y = data_set.user_data['width']
-        resolution_x, resolution_y = data_set.user_data['resolution']
+        width_x, width_y = signal_data.user_data['width']
+        resolution_x, resolution_y = signal_data.user_data['resolution']
 
-        output_data_set = DataSet()
-        output_data_set.user_data = data_set.user_data
-        for _, data_array in data_set.data_arrays.items():
+        output_signal_data = DataSet()
+        output_signal_data.user_data = signal_data.user_data
+        for _, data_array in signal_data.data_arrays.items():
             ProcessSawtooth2D.__check_sample_count_slow_sawtooth(data_array, width_y)
             ProcessSawtooth2D.__check_sample_count_fast_sawtooth(data_array, width_x, resolution_x, resolution_y)
             ProcessSawtooth2D.__check_matching_cuttoff(width_x, width_y, resolution_x, resolution_y)
@@ -33,25 +33,25 @@ class ProcessSawtooth2D(SignalProcessorInterface):
             samples_egde_y = int(sample_count * width_y)
             offsets = np.arange(0, samples_egde_y, samples_sawtooth_x, dtype=np.int)
 
+            identifier = f'{data_array.name}_SawtoothProcessed2D'
             sliced_data = np.array([data_array[o:o + samples_edge_x] for o in offsets])
-            identifier = '{0}_SawtoothProcessed2D'.format(data_array.name)
             result_data = DataArray(identifier, data_array.label, preset_data=sliced_data)
-            output_data_set.add_array(result_data)
+            output_signal_data.add_array(result_data)
 
-        return output_data_set
+        return output_signal_data
 
     @staticmethod
     def __check_sample_count_fast_sawtooth(data_array, width_x, resolution_x, resolution_y):
         expected_pixels_x = resolution_x * width_x
         samples_rising_edge = len(data_array) / resolution_y * width_x
         if not ProcessSawtooth2D.__is_integer(samples_rising_edge) or expected_pixels_x != samples_rising_edge:
-            raise AssertionError('Invalid rising edge X (samples {} not an integer)!'.format(samples_rising_edge))
+            raise AssertionError(f'Invalid rising edge X (samples {samples_rising_edge} not an integer)!')
 
     @staticmethod
     def __check_sample_count_slow_sawtooth(data_array, width_y):
         samples_rising_edge = len(data_array) * width_y
         if not ProcessSawtooth2D.__is_integer(samples_rising_edge):
-            raise AssertionError('Invalid rising edge Y (samples {} not an integer)!'.format(samples_rising_edge))
+            raise AssertionError(f'Invalid rising edge Y (samples {samples_rising_edge} not an integer)!')
 
     @staticmethod
     def __check_matching_cuttoff(width_x, width_y, resolution_x, resolution_y):
@@ -59,7 +59,7 @@ class ProcessSawtooth2D(SignalProcessorInterface):
         pixels_y = resolution_y * width_y
         if ProcessSawtooth2D.__is_integer(pixels_x / pixels_y) or ProcessSawtooth2D.__is_integer(pixels_y / pixels_x):
             return
-        error_message = 'Pixel ratio is incompatible with cuttoff (x={}, y={})!'.format(pixels_x, pixels_y)
+        error_message = f'Pixel ratio is incompatible with cuttoff (x={pixels_x}, y={pixels_y})!'
         raise AssertionError(error_message)
 
     @staticmethod
