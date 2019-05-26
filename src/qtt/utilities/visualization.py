b0d08efbfa2bfefa021eb1dbb03611163e050efc
Pieter Eendebak <pieter.eendebak@gmail.com>
2019-05-26 19:55:37 +0200

    add docstring

--- src/qtt/utilities/visualization.py
+++ src/qtt/utilities/visualization.py
@@ -1,6 +1,8 @@
 from typing import Optional, Any
-import qtt.algorithms.functions
 import matplotlib.pyplot as plt
+import numpy as np
+
+import qtt.algorithms.functions
 
 
 def plot_horizontal_line(x: float, color : str ='c', alpha : float =.5, label : Optional[str] =None) -> Any:
@@ -38,7 +40,13 @@ def plot_vertical_line(x: float, color : str ='c', alpha : float =.5, label : Op
     vline.set_linestyle('--')
     return vline
 
-def plot_double_gaussian_fit(result_dict, xdata):
+def plot_double_gaussian_fit(result_dict : dict, xdata : np.array) -> None:
+    """ Plot a two Gaussians from a double Gaussian fit
+    
+    Args:
+        result_dict: Result of the double Gaussian fitting
+        xdata: Independent data
+    """
     plt.plot(xdata, qtt.algorithms.functions.gaussian(xdata, *result_dict['left']), 'g', label='left' )
     _=plt.plot(xdata, qtt.algorithms.functions.gaussian(xdata, *result_dict['right']), 'r', label='right' )
     
