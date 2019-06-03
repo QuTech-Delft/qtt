0bdbf571f46fd14feaa9ceb4e4ea7ff4ed0568e2
Pieter Eendebak <pieter.eendebak@tno.nl>
2019-06-03 23:34:05 +0200

    add tests

---
+++ src/tests/test_exceptions.py
@@ -0,0 +1,14 @@
+import unittest
+import qtt.exceptions
+
+class TestExceptions(unittest.TestCase):
+
+    def test_calibration_exception(self):
+        self.assertIsInstance(qtt.exceptions.CalibrationException, BaseException)
+
+    def test_PackageVersionWarning(self):
+        self.assertIsInstance(qtt.exceptions.PackageVersionWarning, UserWarning)
+
+    def test_MissingOptionalPackageWarning(self):
+        self.assertIsInstance(qtt.exceptions.MissingOptionalPackageWarning, UserWarning)
+    
