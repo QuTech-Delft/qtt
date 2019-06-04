3f6d69cf7b56becf36fb8cd14d76d69282d96cea
Pieter Eendebak <pieter.eendebak@gmail.com>
2019-06-04 09:29:27 +0200

    fix tests

--- src/tests/test_exceptions.py
+++ src/tests/test_exceptions.py
@@ -1,14 +1,14 @@
 import unittest
 import qtt.exceptions
 
+
 class TestExceptions(unittest.TestCase):
 
     def test_calibration_exception(self):
-        self.assertIsInstance(qtt.exceptions.CalibrationException, BaseException)
+        self.assertTrue(issubclass(qtt.exceptions.CalibrationException, BaseException))
 
     def test_PackageVersionWarning(self):
-        self.assertIsInstance(qtt.exceptions.PackageVersionWarning, UserWarning)
+        self.assertTrue(issubclass(qtt.exceptions.PackageVersionWarning, UserWarning))
 
     def test_MissingOptionalPackageWarning(self):
-        self.assertIsInstance(qtt.exceptions.MissingOptionalPackageWarning, UserWarning)
-
+        self.assertTrue(issubclass(qtt.exceptions.MissingOptionalPackageWarning, UserWarning))
