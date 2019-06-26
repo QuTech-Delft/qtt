60828d78cc4df33f186b365122354ae744811501
CumulonimbusCalvus <9318177+CumulonimbusCalvus@users.noreply.github.com>
2019-06-26 17:02:37 +0200

    [OP-53] Typehints and string formatting.

--- src/qtt/instrument_drivers/virtualAwg/awgs/Tektronix5014C.py
+++ src/qtt/instrument_drivers/virtualAwg/awgs/Tektronix5014C.py
@@ -1,16 +1,18 @@
+from typings import Any, Dict, List, Optional, Tuple
 import numpy as np
 
 from qcodes import Parameter
 from qcodes.utils.validators import Numbers
+from qcodes.instrument_drivers.tektronix.AWG5014 import Tektronix_AWG5014
 from qtt.instrument_drivers.virtualAwg.awgs.common import AwgCommon, AwgCommonError
 
 
 class Tektronix5014C_AWG(AwgCommon):
 
-    def __init__(self, awg):
+    def __init__(self, awg: Tektronix_AWG5014) -> None:
         super().__init__('Tektronix_AWG5014', channel_numbers=[1, 2, 3, 4], marker_numbers=[1, 2])
         if type(awg).__name__ is not self._awg_name:
-            raise AwgCommonError('The AWG does not correspond with {}'.format(self._awg_name))
+            raise AwgCommonError(f'The AWG does not correspond with {self._awg_name}')
         self.__settings = [Parameter(name='sampling_rate', unit='GS/s', initial_value=1.0e9,
                                      vals=Numbers(1.0e7, 1.2e9), set_cmd=None),
                            Parameter(name='marker_delay', unit='ns', initial_value=0.0,
@@ -27,76 +29,77 @@ class Tektronix5014C_AWG(AwgCommon):
         self.__awg = awg
 
     @property
-    def fetch_awg(self):
+    def fetch_awg(self) -> Tektronix_AWG5014:
         return self.__awg
 
-    def run(self):
+    def run(self) -> None:
         self.__awg.run()
 
-    def stop(self):
+    def stop(self) -> None:
         self.__awg.stop()
 
-    def reset(self):
+    def reset(self) -> None:
         self.__awg.reset()
 
-    def enable_outputs(self, channels=None):
+    def enable_outputs(self, channels: Optional[List[int]] = None) -> None:
         if not channels:
             channels = self._channel_numbers
         if not all([ch in self._channel_numbers for ch in channels]):
-            raise AwgCommonError("Invalid channel numbers {}".format(channels))
-        [self.__awg.set('ch{}_state'.format(ch), 1) for ch in channels]
+            raise AwgCommonError(f'Invalid channel numbers {channels}')
+        [self.__awg.set(f'ch{ch}_state', 1) for ch in channels]
 
-    def disable_outputs(self, channels=None):
+    def disable_outputs(self, channels: Optional[List[int]] = None) -> None:
         if not channels:
             channels = self._channel_numbers
         if not all([ch in self._channel_numbers for ch in channels]):
-            raise AwgCommonError("Invalid channel numbers {}".format(channels))
-        [self.__awg.set('ch{}_state'.format(ch), 0) for ch in channels]
+            raise AwgCommonError(f'Invalid channel numbers {channels}')
+        [self.__awg.set(f'ch{ch}_state', 0) for ch in channels]
 
-    def change_setting(self, name, value):
+    def change_setting(self, name: str, value: Any) -> None:
         index = next(i for i, p in enumerate(self.__settings) if p.name == name)
         self.__settings[index].set(value)
 
-    def retrieve_setting(self, name):
+    def retrieve_setting(self, name: str) -> Any:
         index = next(i for i, p in enumerate(self.__settings) if p.name == name)
         return self.__settings[index].get()
 
-    def update_running_mode(self, mode):
+    def update_running_mode(self, mode: str) -> None:
         modes = ['CONT', 'SEQ']
         if mode not in modes:
-            raise AwgCommonError('Invalid AWG mode ({})'.format(mode))
+            raise AwgCommonError(f'Invalid AWG mode ({mode})')
         self.__awg.set('run_mode', mode)
 
-    def retrieve_running_mode(self):
+    def retrieve_running_mode(self) -> float:
         return self.__awg.get('run_mode')
 
-    def update_sampling_rate(self, sampling_rate):
-        self.__awg.set('clock_freq', sampling_rate)
-
-    def retrieve_sampling_rate(self):
+    def retrieve_sampling_rate(self) -> float:
         return self.__awg.get('clock_freq')
 
-    def update_gain(self, gain):
-        [self.__awg.set('ch{0}_amp'.format(ch), gain) for ch in self._channel_numbers]
+    def update_gain(self, gain: float) -> None:
+        [self.__awg.set(f'ch{ch}_amp', gain) for ch in self._channel_numbers]
 
-    def retrieve_gain(self):
-        gains = [self.__awg.get('ch{0}_amp'.format(ch)) for ch in self._channel_numbers]
+    def retrieve_gain(self) -> float:
+        gains = [self.__awg.get(f'ch{ch}_amp') for ch in self._channel_numbers]
         if not all(g == gains[0] for g in gains):
             raise ValueError('Not all channel amplitudes are equal. Please reset!')
         return gains[0]
 
-    def upload_waveforms(self, sequence_names, sequence_channels, sequence_items, reload=True):
+    def upload_waveforms(self, sequence_names: List[str], sequence_channels: List[Tuple[int, ...]],
+                         sequence_items: List[np.ndarray], reload: bool = True) -> None:
         sequences = (sequence_names, sequence_channels, sequence_items)
         channel_data, waveform_data = Tektronix5014C_AWG.create_waveform_data(*sequences)
+        self._waveform_data = waveform_data
+        self._channel_data = channel_data
         if reload:
             self._upload_waveforms(list(waveform_data.keys()), list(waveform_data.values()))
-        self._channel_data = channel_data
+
         self._set_sequence(list(channel_data.keys()), list(channel_data.values()))
 
     @staticmethod
-    def create_waveform_data(names, channels, items):
-        channel_data = dict()
-        waveform_data = dict()
+    def create_waveform_data(names: List[str], channels: List[Tuple[int, ...]],
+                             items: List[np.ndarray]) -> Tuple[Dict[str, int], Dict[int, List[np.ndarry]]]:
+        channel_data: Dict[str, int] = {}
+        waveform_data: Dict[int, List[np.ndarry]] = {}
 
         unique_channels = set([channel for (channel, *_) in channels])
         for unique_channel in unique_channels:
@@ -114,12 +117,12 @@ class Tektronix5014C_AWG(AwgCommon):
             waveform_data[data_name] = data
             channel_data.setdefault(unique_channel, []).append(data_name)
 
-        return channel_data,waveform_data
+        return channel_data, waveform_data
 
-    def retrieve_waveforms(self):
+    def retrieve_waveforms(self) -> Dict[str, List[np.ndarray]]:
         return self._waveform_data if self._waveform_data else None
 
-    def _set_sequence(self, channels, sequence):
+    def _set_sequence(self, channels: List[int], sequence: List[str]) -> None:
         if not sequence or len(sequence) != len(channels):
             raise AwgCommonError('Invalid sequence and channel count!')
         if not all(len(idx) == len(sequence[0]) for idx in sequence):
@@ -138,7 +141,8 @@ class Tektronix5014C_AWG(AwgCommon):
                     self.__awg.set_sqel_waveform("", channel, row_index + 1)
         self.__awg.set_sqel_goto_state(request_rows, 1)
 
-    def _upload_waveforms(self, names, waveforms, file_name='default.awg'):
+    def _upload_waveforms(self, names: List[str], waveforms: List[np.ndarray],
+                          file_name: str = 'default.awg') -> None:
         pack_count = len(names)
         packed_waveforms = dict()
         [wfs, m1s, m2s] = list(map(list, zip(*waveforms)))
@@ -172,18 +176,18 @@ class Tektronix5014C_AWG(AwgCommon):
         current_dir = self.__awg.visa_handle.query('MMEMory:CDIRectory?')
         current_dir = current_dir.replace('"', '')
         current_dir = current_dir.replace('\n', '\\')
-        self.__awg.load_awg_file('{0}{1}'.format(current_dir, file_name))
+        self.__awg.load_awg_file(f'{current_dir}{file_name}')
 
-    def delete_waveforms(self):
+    def delete_waveforms(self) -> None:
         self.__awg.delete_all_waveforms_from_list()
         self._waveform_data = None
 
-    def __set_sequence_length(self, count):
-        self.__awg.write('SEQuence:LENGth {0}'.format(count))
+    def __set_sequence_length(self, count: int) -> None:
+        self.__awg.write(f'SEQuence:LENGth {count}')
 
-    def __delete_sequence(self):
+    def __delete_sequence(self) -> None:
         self.__set_sequence_length(0)
 
-    def __get_sequence_length(self):
+    def __get_sequence_length(self) -> int:
         row_count = self.__awg.ask('SEQuence:LENGth?')
         return int(row_count)
