064fef6e50e5cf1673342f3961eb305e20c86e88
Ammar <ammar.abdulamir@gmail.com>
2019-06-26 10:58:06 +0200

    Some more coverage

--- src/tests/unittests/instrument_drivers/virtualAwg/test_virtual_awg.py
+++ src/tests/unittests/instrument_drivers/virtualAwg/test_virtual_awg.py
@@ -1,9 +1,27 @@
 import unittest
 from qcodes import Instrument
+
+from qtt.instrument_drivers.virtualAwg.settings import SettingsInstrument
 from qtt.instrument_drivers.virtualAwg.virtual_awg import VirtualAwg
 
 
+class QuantumDeviceSettings(Instrument):
+
+    def __init__(self):
+        super().__init__('settings')
+        self.awg_map = {
+            'P1': (0, 1),
+            'P2': (0, 2),
+            'dig_mk': (0, 1, 1)
+        }
+
+
 class TestVirtualAwg(unittest.TestCase):
+    def setUp(self) -> None:
+        self.settings = SettingsInstrument('')
+
+    def tearDown(self) -> None:
+        self.settings.close()
 
     def test_init_HasNoErrors(self):
         from unittest.mock import Mock
@@ -11,18 +29,13 @@ class TestVirtualAwg(unittest.TestCase):
         type(awg_driver).__name__ = 'Tektronix_AWG5014'
         awgs = [awg_driver]
 
-        class QuantumDeviceSettings(Instrument):
-
-            def __init__(self):
-                super().__init__('settings')
-                self.awg_map = {
-                    'P1': (0, 1),
-                    'P2': (0, 2),
-                    'dig_mk': (0, 1, 1)
-                }
-
-        settings = QuantumDeviceSettings()
-        virtual_awg = VirtualAwg(awgs, settings)
+        virtual_awg = VirtualAwg(awgs, self.settings)
+        self.assertEqual(virtual_awg.settings, self.settings)
         self.assertEqual(awg_driver, virtual_awg.awgs[0].fetch_awg)
         virtual_awg.close()
-        settings.close()
+
+    def test_init_HasNoInstruments(self):
+        virtual_awg = VirtualAwg(settings=self.settings)
+        self.assertEqual(virtual_awg.settings, self.settings)
+        self.assertEqual(virtual_awg.instruments, [])
+        virtual_awg.close()
