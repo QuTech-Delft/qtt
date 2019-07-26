e096692e0232882720d4eada92e3f0c2034efe45
Pieter Eendebak <pieter.eendebak@gmail.com>
2019-07-27 00:12:05 +0200

    add tests

--- src/tests/unittests/test_dataset_processing.py
+++ src/tests/unittests/test_dataset_processing.py
@@ -2,7 +2,7 @@ import unittest
 import qcodes.tests.data_mocks
 import numpy as np
 
-from qtt.dataset_processing import slice_dataset, process_dataarray, average_dataset, dataset_dimension, average_multirow_dataset
+from qtt.dataset_processing import slice_dataset, process_dataarray, average_dataset, dataset_dimension, average_multirow_dataset, resample_dataset
 
 
 class TestDataProcessing(unittest.TestCase):
@@ -18,7 +18,7 @@ class TestDataProcessing(unittest.TestCase):
 
         dataset_sliced = slice_dataset(dataset, [1, 5], axis=1)
         np.testing.assert_array_equal(dataset_sliced.z, np.array(
-            [[1,  4],  [2,  5],       [5,  8],       [10, 13],       [17, 20],       [26, 29]]))
+            [[1, 4], [2, 5], [5, 8], [10, 13], [17, 20], [26, 29]]))
 
     def test_process_dataarray(self):
         dataset = qcodes.tests.data_mocks.DataSet1D()
@@ -27,6 +27,14 @@ class TestDataProcessing(unittest.TestCase):
         processed_dataset = process_dataarray(dataset, input_name, output_name, lambda x: x + 5)
         self.assertIn(output_name, processed_dataset.arrays)
         np.testing.assert_array_equal(np.array(getattr(dataset, input_name)) + 5, dataset.arrays[output_name])
+
+    def test_process_dataarray_inplace(self):
+        dataset = qcodes.tests.data_mocks.DataSet1D()
+        input_name = 'y'
+        output_name = None
+        processed_dataset = process_dataarray(dataset, input_name, output_name, lambda x: x + 5)
+        self.assertEqual(set(processed_dataset.arrays.keys()), {'x_set', 'y'})
+        np.testing.assert_array_equal(np.array(getattr(dataset, input_name)) + 5, np.array([8., 9., 10., 11., 12.]))
 
     def test_average_dataset(self):
         dataset2d = qcodes.tests.data_mocks.DataSet2D()
@@ -51,6 +59,11 @@ class TestDataProcessing(unittest.TestCase):
         np.testing.assert_array_equal(averaged_dataset.signal, np.array(
             [[0.5, 1.5, 4.5, 9.5], [6.5, 7.5, 10.5, 15.5], [20.5, 21.5, 24.5, 29.5]]))
 
+        averaged_dataset = average_multirow_dataset(dataset2d, 2, [-1, -2, -3])
+        np.testing.assert_array_equal(averaged_dataset.x, [-1, -2, -3])
+        with self.assertRaises(AssertionError):
+            np.testing.assert_array_equal(averaged_dataset.x, [-1, -2, -3, -4])
+
     def test_slice_dataset_copy_metadata(self):
         dataset = qcodes.tests.data_mocks.DataSet2D()
         dataset.metadata['a'] = 1
@@ -59,3 +72,18 @@ class TestDataProcessing(unittest.TestCase):
 
         dataset_sliced = slice_dataset(dataset, [2, 5], copy_metadata=True)
         self.assertDictEqual(dataset_sliced.metadata, dataset.metadata)
+
+    def test_resample_dataset(self):
+        dataset1d = qcodes.tests.data_mocks.DataSet1D()
+        dataset2d = qcodes.tests.data_mocks.DataSet2D()
+
+        with self.assertRaises(ValueError):
+            d = resample_dataset(dataset1d, (0, 1))
+        d = resample_dataset(dataset1d, (4,))
+        self.assertEqual(d.y.shape, (2, ))
+
+        d = resample_dataset(dataset2d, (2, 1))
+        np.testing.assert_array_equal(d.z, np.array([[0, 1, 4, 9], [4, 5, 8, 13], [16, 17, 20, 25]]))
+
+        d = average_dataset(dataset2d, axis=1)
+        self.assertEqual(d.z.shape, (6, ))
