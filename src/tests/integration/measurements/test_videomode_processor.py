f130e3e6af3402ec306f78fef49c5e593cd120c2
Pieter Eendebak <pieter.eendebak@gmail.com>
2019-08-20 22:57:04 +0200

    fix tests

--- src/tests/integration/measurements/test_videomode_processor.py
+++ src/tests/integration/measurements/test_videomode_processor.py
@@ -1,4 +1,5 @@
 import unittest
+import pyqtgraph
 
 import qcodes
 import qtt.data
@@ -10,17 +11,17 @@ from qtt.measurements.videomode import VideoMode
 
 class TestVideoModeProcessor(unittest.TestCase):
 
-
     def test_DummyVideoModeProcessor(self):
+        qt_application = pyqtgraph.mkQApp()
+
         station = qtt.simulation.virtual_dot_array.initialize()
         dummy_processor = DummyVideoModeProcessor(station)
         vm = VideoMode(station, Naverage=25, diff_dir=None, verbose=1,
                        nplots=1, dorun=False, videomode_processor=dummy_processor)
-        vm.stopreadout()
-        vm.stop()
         vm.updatebg()
         datasets = vm.get_dataset()
         vm.close()
         qtt.simulation.virtual_dot_array.close()
         self.assertIsInstance(datasets[0], qcodes.DataSet)
 
+        qt_application.processEvents()
