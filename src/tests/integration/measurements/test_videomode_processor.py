6149f6cd845bd36ab96e43d49547bd932da5f389
Pieter Eendebak <pieter.eendebak@gmail.com>
2019-08-21 16:44:27 +0200

    process all qt events

--- src/tests/integration/measurements/test_videomode_processor.py
+++ src/tests/integration/measurements/test_videomode_processor.py
@@ -1,4 +1,6 @@
 import unittest
+import pyqtgraph
+import time
 
 import qcodes
 import qtt.data
@@ -12,6 +14,8 @@ class TestVideoModeProcessor(unittest.TestCase):
 
 
     def test_DummyVideoModeProcessor(self):
+        qtapp = pyqtgraph.mkQApp()
+        
         station = qtt.simulation.virtual_dot_array.initialize()
         dummy_processor = DummyVideoModeProcessor(station)
         vm = VideoMode(station, Naverage=25, diff_dir=None, verbose=1,
@@ -23,4 +27,7 @@ class TestVideoModeProcessor(unittest.TestCase):
         vm.close()
         qtt.simulation.virtual_dot_array.close()
         self.assertIsInstance(datasets[0], qcodes.DataSet)
+        
+        qtapp.processEvents()
+        time.sleep(.1)
 
