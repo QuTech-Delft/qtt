fix covariance calculation

src/qtt/algorithms/allxy.py
@@ -78,7 +78,7 @@ def fit_allxy(dataset: DataSet, initial_parameters: Optional[np.ndarray] = None)
     param_names = lmfit_model.param_names
     result = lmfit_model.fit(allxy_data, x=x_data, **dict(zip(param_names, initial_parameters)), verbose=0)
     fitted_parameters = np.array([result.best_values[p] for p in param_names])
-    fitted_parameters_covariance = np.sqrt(np.diag(result.covar))
+    fitted_parameters_covariance = np.diag(result.covar)
     chi_squared = result.chisqr
 
     return {'fitted_parameters': fitted_parameters, 'description': 'allxy fit', 'initial_parameters': initial_parameters, 'fitted_parameters_covariance': fitted_parameters_covariance, 'chi_squared': chi_squared}