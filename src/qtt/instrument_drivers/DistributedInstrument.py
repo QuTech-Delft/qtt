75360e0d887fffa52321f56d15ff3e887a6fd0d8
Bogdan Constantin <bogdan.v.constantin@gmail.com>
2020-03-20 12:14:22 +0100

    [DEM-1368] initial investigation and destructor

--- src/qtt/instrument_drivers/DistributedInstrument.py
+++ src/qtt/instrument_drivers/DistributedInstrument.py
@@ -42,6 +42,9 @@ class InstrumentDataClient(Instrument):
                           default_value=default_value, params=params)
         self.add_parameter(name, unit=unit, get_cmd=command,
                            docstring=doc_string)
+    
+    def __del__(self):
+        self._client_.destroy()
 
 # -----------------------------------------------------------------------------
 
