ab65bd99d181031f6debe5e5b1c6fffc78810f90
Pieter Eendebak <pieter.eendebak@gmail.com>
2020-04-01 20:27:48 +0200

    formatting

--- src/qtt/algorithms/functions.py
+++ src/qtt/algorithms/functions.py
@@ -356,8 +356,8 @@ def fit_gauss_ramsey(x_data, y_data, weight_power=None, maxiter=None, maxfun=500
         initial_parameters = initial_params
     lmfit_model = Model(gauss_ramsey_model)
     lmfit_model.set_param_hint('amplitude', min=0)
-    lmfit_result = lmfit_model.fit(
-        y_data, x=x_data, **dict(zip(lmfit_model.param_names, initial_parameters)), verbose=verbose >= 2, weights = weights)
+    lmfit_result = lmfit_model.fit(y_data, x=x_data, **dict(zip(lmfit_model.param_names, initial_parameters)),
+                                   verbose=verbose >= 2, weights = weights)
 
     import qtt.algorithms.fitting
     result_dict = qtt.algorithms.fitting.extract_lmfit_parameters(lmfit_model, lmfit_result)
