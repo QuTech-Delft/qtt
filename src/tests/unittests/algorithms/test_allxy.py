19323c25f4e1326ffa4981f675811655de618443
QFer <f.j.grooteman@tudelft.nl>
2021-03-15 11:29:50 +0100

    Solved some pep-warnings

--- src/tests/unittests/algorithms/test_allxy.py
+++ src/tests/unittests/algorithms/test_allxy.py
@@ -9,12 +9,12 @@ from qtt.algorithms.allxy import fit_allxy, plot_allxy, allxy_model
 
 class TestAllxy(unittest.TestCase):
 
-    def test_allxy(self, fig = None):
+    def test_allxy(self, fig=None):
         dataset = makeDataSet1Dplain('index', np.arange(21), 'allxy', [0.12, 0.16533333, 0.136, 0.17066666, 0.20266667,
-                                                                       0.452, 0.48133334, 0.58666666, 0.43199999, 0.52933334,
-                                                                       0.44533333, 0.51066667, 0.46, 0.48133334, 0.47066667,
-                                                                       0.47333333, 0.488, 0.80799999, 0.78933333, 0.788,
-                                                                       0.79333333])
+                                                                       0.452, 0.48133334, 0.58666666, 0.43199999,
+                                                                       0.52933334, 0.44533333, 0.51066667, 0.46,
+                                                                       0.48133334, 0.47066667, 0.47333333, 0.488,
+                                                                       0.80799999, 0.78933333, 0.788, 0.79333333])
         result = fit_allxy(dataset)
         self.assertIsInstance(result, dict)
         np.testing.assert_array_almost_equal([0.1, 0, .5, 0, .9, 0], result['fitted_parameters'], decimal=1)
@@ -23,14 +23,14 @@ class TestAllxy(unittest.TestCase):
             plt.close(fig)
 
     def test_allxy_covariance_regression(self):
-        allxy_data =[0.175, 0.24166666666666667, 0.23166666666666666, 0.2, 0.21, 0.49666666666666665,
-                     0.5316666666666666, 0.545, 0.53, 0.605, 0.5466666666666666, 0.58, 0.505, 0.5916666666666667, 0.6,
-                     0.53, 0.5716666666666667, 0.9016666666666666, 0.91, 0.905, 0.9033333333333333]
-        dataset = makeDataSet1Dplain('index', np.arange(21), 'allxy',allxy_data)
+        allxy_data = [0.175, 0.24166666666666667, 0.23166666666666666, 0.2, 0.21, 0.49666666666666665,
+                      0.5316666666666666, 0.545, 0.53, 0.605, 0.5466666666666666, 0.58, 0.505, 0.5916666666666667, 0.6,
+                      0.53, 0.5716666666666667, 0.9016666666666666, 0.91, 0.905, 0.9033333333333333]
+        dataset = makeDataSet1Dplain('index', np.arange(21), 'allxy', allxy_data)
         result = fit_allxy(dataset)
         self.assertTrue(result['fitted_parameters_covariance'] is not None)
-        self.assertAlmostEqual(result['reduced_chi_squared'], 0.000976, places=5 )
-        self.assertAlmostEqual(result['fitted_parameters_covariance'][0], 1.95305266e-04, places=5 )
+        self.assertAlmostEqual(result['reduced_chi_squared'], 0.000976, places=5)
+        self.assertAlmostEqual(result['fitted_parameters_covariance'][0], 1.95305266e-04, places=5)
 
     def test_allxy_model(self):
         offsets = [.0, .1, .2]
