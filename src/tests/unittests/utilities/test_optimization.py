359a0169321a50c6ce40dce74b246db854f30131
Pieter Eendebak <pieter.eendebak@tno.nl>
2021-08-30 09:56:43 +0200

    fix ci

--- src/tests/unittests/utilities/test_optimization.py
+++ src/tests/unittests/utilities/test_optimization.py
@@ -1,10 +1,12 @@
 import unittest
 
+import lmfit
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas
 import scipy.optimize
 
+from qtt.algorithms.functions import linear_function
 from qtt.utilities.optimization import (AverageDecreaseTermination,
                                         OptimizerCallback)
 
@@ -29,6 +31,12 @@ class TestOptimizationUtilities(unittest.TestCase):
         self.assertEqual(oc.number_of_evaluations(), result.nit)
         self.assertIsInstance(oc.data, pandas.DataFrame)
 
+        oc.clear()
+
+        lmfit_model = lmfit.Model(linear_function, independent_vars=['x'])
+        xdata = np.linspace(0, 10, 40)
+        data = 10+5*xdata
+        lmfit_model.fit(data, x=xdata, a=1, b=1, iter_cb=oc.lmfit_callback)
         plt.figure(100)
         plt.clf()
         oc.plot(logy=True)
@@ -36,5 +44,5 @@ class TestOptimizationUtilities(unittest.TestCase):
 
     def test_AverageDecreaseTermination(self,):
         tc = AverageDecreaseTermination(4)
-        results = [tc(0, value) for value in [4, 3, 2, 1, .1, 0, 0, 0, 0, 0.01, 0]]
+        results = [tc(0, None, value, None, None) for value in [4, 3, 2, 1, .1, 0, 0, 0, 0, 0.01, 0]]
         self.assertEqual(results, [False, False, False, False, False, False, False, False, False, True, True])
