9ef57bf9214edf5cf84713c637925c4b89d793b8
Pieter Eendebak <pieter.eendebak@tno.nl>
2022-03-14 14:45:17 +0100

    add representation method

--- src/qtt/qiskit/passes.py
+++ src/qtt/qiskit/passes.py
@@ -37,6 +37,10 @@ class RemoveGateByName(TransformationPass):
 
         return dag
 
+    def __repr__(self) -> str:
+        name = self.__class__.__module__ + '.'+self.__class__.__name__
+        return f"<{name} at 0x{id(self):x}: gate {self._gate_name}"
+
 
 class RemoveSmallRotations(TransformationPass):
     """Return a circuit with small rotation gates removed."""
